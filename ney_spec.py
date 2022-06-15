5998bfd319e41dcb3a970fb63d53ed57e15ede5f
Yoones Mahdian <MahdianYoones@gmail.com>
2022-06-15 12:02:44 +0430

    Sets new specifications for the desired Ney according to new design

--- ney_spec.py
+++ ney_spec.py
@@ -1,38 +1,17 @@
 # Specification of the desired Ney
-desired_ney = {
 
-	# The length of each hole. h7 is equal with the length of entire Ney
-	"h1":			round(524.4),
-	"h2":			round(467.2),
-	"h3":			round(428.4),
-	"h4":			round(392.9),
-	"h5":			round(370.8),
-	"h6":			round(350.0),
-	"h7":			round(311.8),
-	
-	# Specifications related to the top node
-	"mp_lenght":		10,			# mouthpiece length
-	"h_diameter":		10,			# holes diameter
-	"n1_diameter":		18,			# diamter at top
-	
-	"min_chunk_l":		20,			# minimum chunk length
-	
-	"min_hj_dist":		10,			# minimum distance between holes and junctions
-	"min_hh_dist":		10,			# minimum distance between holes
-	
-	"diam_diff_lower":	0.5,			# lower bound for diameter diff between adjacent nodes
-	"diam_diff_upper":	1.5,			# upper bound for that!
-	
-	# The following bounds are driven from two constraints:
-	# 	1. Node 2 must be double the length of Node 1
-	# 	2. Nodes 3 to 6 must increase in length (at least 1mm)
-	
-	"n1_llower":		36.5,		# lower bound for the length of the top node
-								# without considering the length of the mouthpiece
-	"n1_lupper":		(round(524.4) - 263) / 3, # = 87mm
-	
-	"n3_llower":		72,			# third node
-	"n4_llower":		71,			# fourth node
-	"n5_llower":		70,			# fifth node
-	"n6_llower":		30			# sixth node
+desired_ney = {
+	"total_len":		round(524.4),	# total height, including the mouthpiece
+	"h1_len":			round(467.2),	# 1st hole from top
+	"h2_len":			round(428.4),	# 2nd hole from top
+	"h3_len":			round(392.9),	# 3rd hole from top
+	"h4_len":			round(370.8),	# 4th hole from top
+	"h5_len":			round(350.0),	# 5th hole from top
+	"h6_len":			round(311.8),	# 6th hole from top
+	"mp_len":			10,			# mouthpiece length
+	"hole_diam":		10,			# holes diameter
+	"top_diam":		(18, 24),		# diamter at top
+	"min_len":		20,			# minimum chunk length
+	"h_margin":		10,			# minimum hole/hole and hole/junction space
+	"ddiff":			(0.5, 1.5),	# diameter difference between adjacent nodes
 }	

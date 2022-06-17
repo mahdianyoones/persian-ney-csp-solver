074fec50614eff922aa799d187e10ef82463853f
Yoones Mahdian <MahdianYoones@gmail.com>
2022-06-17 14:56:15 +0430

    renames specification keys

--- ney_spec.py
+++ ney_spec.py
@@ -1,17 +1,17 @@
 # Specification of the desired Ney
 
 spec = {
-	"total_len":		round(524.4),	# total height, including the mouthpiece
-	"h1_len":			round(467.2),	# 1st hole from top
-	"h2_len":			round(428.4),	# 2nd hole from top
-	"h3_len":			round(392.9),	# 3rd hole from top
-	"h4_len":			round(370.8),	# 4th hole from top
-	"h5_len":			round(350.0),	# 5th hole from top
-	"h6_len":			round(311.8),	# 6th hole from top
-	"mp_len":			10,			# mouthpiece length
-	"hole_diam":		10,			# holes diameter
-	"top_diam":		(18, 24),		# diamter at top
-	"min_len":		20,			# minimum chunk length
-	"h_margin":		10,			# minimum hole/hole and hole/junction space
-	"ddiff":			(0.5, 1.5),	# diameter difference between adjacent nodes
+	"len":			round(524.4),				# total height, including the mouthpiece
+	"h1":			round(467.2),				# 1st hole from top
+	"h2":			round(428.4),				# 2nd hole from top
+	"h3":			round(392.9),				# 3rd hole from top
+	"h4":			round(370.8),				# 4th hole from top
+	"h5":			round(350.0),				# 5th hole from top
+	"h6":			round(311.8),				# 6th hole from top
+	"mp":			10,						# mouthpiece length
+	"holed":			10,						# holes diameter
+	"topd":			{"min": 18, "max": 24},		# diamter at top
+	"minl":			20,						# minimum chunk length
+	"hmarg":			10,						# minimum hole/hole and hole/junction space
+	"ddiff":			{"min": 0.5, "max": 1.5},	# diameter difference between adjacent nodes
 }	

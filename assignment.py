be470b6aefd5ed5872862dd6f1287160e85e3837
Yoones Mahdian <MahdianYoones@gmail.com>
2022-06-21 22:29:42 +0430

    uses constants for set/not set states of features in asignment class

--- assignment.py
+++ assignment.py
@@ -1,3 +1,5 @@
+FEATURE_IS_NOT_SET = False
+FEATURE_IS_SET = True
 class assignment():
 	def __init__(self, csp):
 		self.X = csp.getVars()
@@ -6,7 +8,12 @@ class assignment():
 		self.assigned = [] # order matters
 		self.nodes = {}
 		for i in [1, 2, 3, 4, 5, 6, 7]:			
-			self.nodes[i] = {"D": None, "R": None, "TH": None, "L": None}
+			self.nodes[i] = {
+				"D": 	FEATURE_IS_NOT_SET, 
+				"R": 	FEATURE_IS_NOT_SET, 
+				"TH": 	FEATURE_IS_NOT_SET, 
+				"L": 	FEATURE_IS_NOT_SET
+			}
 		
 	def isComplete(self)
 		if len(self.unassigned) == 0:
@@ -32,3 +39,4 @@ class assignment():
 		del self.asmnt[var]
 		self.unassigned.add(var)
 		del self.assigned[self.assigned.index(var)]
+		self.nodes[int(var[1])][var[0]] = False		

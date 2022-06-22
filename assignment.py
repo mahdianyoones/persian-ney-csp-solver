b4b62e261d2a01ebf1859085206c94ab09420ce8
Yoones Mahdian <MahdianYoones@gmail.com>
2022-06-22 13:30:19 +0430

    removes unnecessary methods from assignments and rename some others

--- assignment.py
+++ assignment.py
@@ -1,9 +1,9 @@
 FEATURE_IS_NOT_SET = False
 FEATURE_IS_SET = True
-class assignment():
+class ASSIGNMENT():
 	def __init__(self, csp):
 		self.X = csp.getVars()
-		self.asmnt = {}
+		self.assignment = {}
 		self.unassigned = self.X.copy()
 		self.assigned = [] # order matters
 		self.nodes = {}
@@ -14,29 +14,20 @@ class assignment():
 				"TH": 	FEATURE_IS_NOT_SET, 
 				"L": 	FEATURE_IS_NOT_SET
 			}
-		
-	def isComplete(self)
+	
+	def is_complete(self)
 		if len(self.unassigned) == 0:
 			return True
 		return False
-		
-	def getAssignment(self):
-		return self.asmnt
-	
-	def getAssigned(self):
-		return self.assigned
-	
-	def getUnassigned(self):
-		return self.unassigned
-		
+			
 	def assign(self, var, val):
-		self.asmnt[var] = val
+		self.assignment[var] = val
 		self.unassigned.remove(var)
 		self.assigned.append(var) # order matters
 		self.nodes[int(var[1])][var[0]] = True
 		
 	def unassign(self, var):
-		del self.asmnt[var]
+		del self.assignment[var]
 		self.unassigned.add(var)
 		del self.assigned[self.assigned.index(var)]
 		self.nodes[int(var[1])][var[0]] = False		

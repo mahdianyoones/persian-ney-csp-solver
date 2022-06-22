4677ebe4e5b95782873cc28f28620fa3e81f062b
Yoones Mahdian <MahdianYoones@gmail.com>
2022-06-23 00:24:50 +0430

    adds test for catalog

--- test.py
+++ test.py
@@ -1,131 +1,48 @@
 import unittest
-from constraints import *
-from consistency import *
-from csp import init_csp
+from catalog import CATALOG
 
-def value(th = 0, r = 0, d = 0, l = 0, no = 0):
-	return (no, l, th, r, d)
-
-class TestConstraints(unittest.TestCase):
-
-	def test_nodes_similar(self):
-		asmnt = [
-			("A", value(th=2.0,r=0.0)),
-			("B1", value(th=2.0,r=0.0)),
-			("C1", value(th=2.0,r=0.0)),
-			("D1", value(th=2.0,r=0.0)),
-			("E1", value(th=2.0,r=0.0)),
-			("F1", value(th=2.0,r=0.0)),
-			("G", value(th=2.0,r=0.0))
-		]
-		res = satisfies("nodes_similar", asmnt)
-		self.assertTrue(res[0])
-		
-		asmnt = [
-			("A", value(th=2.0,r=0.0)),
-			("B1", value(th=2.0,r=0.0)),
-			("C1", value(th=2.0,r=0.0)),
-			("D1", value(th=2.0,r=0.0)),
-			("E1", value(th=2.0,r=1.0)),
-			("F1", value(th=2.0,r=0.0)),
-			("G", value(th=2.0,r=0.0))
-		]
-		res = satisfies("nodes_similar", asmnt)
-		self.assertFalse(res[0])
-		
-		asmnt = [
-			("A", value(th=2.0,r=0.0)),
-			("B1", value(th=2.0,r=0.0)),
-			("D1", value(th=2.0,r=0.0)),
-			("E1", value(th=2.0,r=0.0)),
-		]
-		res = satisfies("nodes_similar", asmnt)
-		self.assertTrue(res[0])
-
-		asmnt = [
-			("A", value(th=2.1,r=0.0)),
-			("B1", value(th=2.0,r=0.0)),
-			("D1", value(th=2.0,r=0.0)),
-			("E1", value(th=2.0,r=0.0)),
-		]
-		res = satisfies("nodes_similar", asmnt)
-		self.assertFalse(res[0])
-		
-	def test_diam_diff(self):
-		asmnt = [
-			("A", value(d=17.5)),
-			("B1", value(d=18.0))
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (False, {"A", "B1"}))
-		
-		asmnt = [
-			("A", value(d=18.0)),
-			("B1", value(d=18.0))
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (False, {"A", "B1"}))
-		
-		asmnt = [
-			("A", value(d=18.5)),
-			("B1", value(d=18.0))
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (True, None))
-		
-		asmnt = [
-			("B1", value(d=18.0)),
-			("A", value(d=18.5)),
-			("C1", value(d=17.5))
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (True, None))
-		
-		asmnt = [
-			("A", value(d=18.5)),
-			("B1", value(d=18.0)),
-			("C1", value(d=18.0))
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (False, {"B1", "C1"}))
-		
-		asmnt = [
-			("B1", value(d=18.0))
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (True, None))
-		
-		asmnt = [
-			("B1", value(d=18.0)),
-			("D1", value(d=17.5)),
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (True, None))
-		
-		asmnt = [
-			("F1", value(d=18.0)),
-			("G", value(d=17.5)),
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (True, None))
-		
-		asmnt = [
-			("F1", value(d=18.0)),
-			("G", value(d=17.5)),
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (True, None))
-		
-		asmnt = [
-			('A', value(no='175',l=27,th=2.5,r=0.0,d=18.0)),
-			('B1', value(no='478',l=20,th=2.5,r=0.0,d=19.0))
-		]
-		self.assertEqual(satisfies("diam_diff", asmnt), (False, {"A", "B1"}))
+class TestCatalog(unittest.TestCase):
 
 
-class TestConsistency(unittest.TestCase):
-
-	def test_is_conistent(self):
-		csp = init_csp()
-		make_A_consistent(csp)
-		self.assertEqual(len(csp["D"]["A"]), 666)
-		
-		asmnt = [
-			('A', value(no='10',l=10,th=2.0,r=0.0,d=18.0)),
+	def test_test1(self):
+		self.catalog = CATALOG("measures_of_drained_pieces.csv")
+		er = [
+			({"1.0", "1.5", "0.5"}, ("R", {"D": "18.0", "TH": "3.0"})),
+			({"0.0", "0.5", "1.0", "1.5"}, ("R", {"TH": "1.0"})),
+			({"18.0", "25.0", "19.0"}, ("D", {"TH": "3.0", "R": "0.5"})),
+			({"13.5", "14.5", "15.0", "16.0"}, ("D", {"TH": "1.0", "R": "1.0"})),
+			({"3.5", "2.5"}, ("TH", {"R": "1.5", "D": "17.5"}))
 		]
-		val = value(no='10',l=10,th=2.5,r=1.0,d=18.0)
-		self.assertEqual(is_consistent(csp, asmnt, "B1", val), (False, {"A", "B1"}))
-		
+		for _er in er:
+			expected = _er[0]
+			key = _er[1][0]
+			filters = _er[1][1]
+			returned = self.catalog.values(key, filters)
+			self.assertEqual(expected, returned)
+
+		er = [
+			(441, {"TH": "1.0"}),
+			(215, {"TH": "1.0", "R": "1.0"}),
+			(40, {"TH": "1.0", "R": "1.5"}),
+			(120, {"TH": "1.0", "R": "1.0", "D": "13.5"}),
+			(7323, {"TH": "2.0"}),
+			(578, {"TH": "2.0", "R": "1.0", "D": "21.0"}),
+			(55, {"TH": "3.0", "R": "0.5", "D": "19.0"}),
+			(60, {"TH": "3.5", "R": "1.5", "D": "17.5"}),
+			(60, {"TH": "3.5", "R": "1.5"}),
+			(60, {"TH": "3.5"}),
+			(2661, {"R": "0.0"}),
+			(185, {"D": "21.5"}),
+			(26, {"D": "23.5"}),
+			(47, {"D": "21.5", "R": "0.0"}),
+			(138, {"D": "21.5", "TH": "2.0", "R": "0.5"}),
+			(0, {"D": "87.5", "TH": "2.0", "R": "0.5"}),
+		]
+		for _er in er:
+			expected = _er[0]
+			filters = _er[1]
+			returned = self.catalog.get_l(filters)
+			self.assertEqual(expected, returned)
+
 if __name__ == '__main__':
 	unittest.main()

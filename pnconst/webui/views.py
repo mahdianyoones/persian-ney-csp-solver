f45f847111888d3c2fec22d35497bc20d4959eb0
Yoones Mahdian <MahdianYoones@gmail.com>
2022-09-27 03:35:53 +0330

    Adds reed adder form and displays them in home

--- pnconst/webui/views.py
+++ pnconst/webui/views.py
@@ -1,8 +1,9 @@
 from multiprocessing import context
 from re import template
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Reedpiece
 
 def index(request):
@@ -12,3 +13,18 @@ def index(request):
     }
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
+
+def add(request):
+    template = loader.get_template("add.html")
+    return HttpResponse(template.render({}, request))
+
+def addreed(request):
+    la = request.POST['label']
+    le = request.POST['length']
+    d = request.POST['diam']
+    t = request.POST['thickness']
+    r = request.POST['roundness']
+    pr =  Reedpiece(label=la, length=le, thickness=t, \
+        roundness=r, diam=d)
+    pr.save()
+    return HttpResponseRedirect(reverse('index'))

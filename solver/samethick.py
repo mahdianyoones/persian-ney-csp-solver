1efbd0ddd30059ed574efbe2349706e5b26ee657
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-02 08:19:49 +0330

    Removes extra comments

--- solver/samethick.py
+++ solver/samethick.py
@@ -4,41 +4,7 @@ class SAMETHICK():
     '''Applies same thickness constraints.
     
     Te variables T1 Trought T7 must have the same value in the
-    final solution.
-
-    T1 through T7 must be assigned the same value. If at any point, no
-    intersection between the domains of these variables exists, contradiction
-    has been occured.
-
-    Having no intersection between the domains means that the constraint
-    cannot be satisfied.
-
-    However, given that T1 is assigned first before other T variables, we
-    can implement a simpler and more efficient algorithm.
-
-    Upon assigning any value to T1, we clean the domain of other T variables
-    and only leave the assigned value.
-
-    For example, if T1 is assigned 0.5mm and other domains are:
-
-    T2 = {0, 0.5, 1, 1.5}
-    T3 = {0.5, 1, 1.5, 2}
-    T4 = {0.5, 2}
-    and etc
-
-    the domains will be come as such:
-
-    T2 = {0.5}
-    T3 = {0.5}
-    T4 = {0.5}
-    and etc
-
-    and the contradiction can be easily detected if the assigned value cannot
-    be found in the current domain of any T variable.
-
-    Note that after assignment of T1 and consistency, propagation does not 
-    occur since the domains contain only one value. If any other algorithm
-    removes this single value, it issues contradiction itself.'''
+    final solution.'''
     
     def propagate(self, csp, reduced_vars):
         '''Establishes indirect consistency W.R.T. same_th.'''

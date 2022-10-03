8f34f21c27d798cee095d01ccabdfa3735a24117
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-03 07:37:33 +0330

    Makes half test cases pass

--- solver/half.py
+++ solver/half.py
@@ -2,125 +2,83 @@ from constants import *
 import math
 
 class HALF():
-	'''Implements consistency algorithm for l1_half_l2 constraint.'''
-
-	def __init__(self, csp):
-		self.__csp = csp
+	'''Implements consistency algorithm for l1_half_l2 constraint.
 	
-	def __new_L1_domain(self, d1, d2):
-		'''Determines new bounds for L1 W.R.T. l1_half_l2 constraint.
-		
-		If new consistent bounds exists, it returns the new legal domain.
-		This is a mathematical function.'''
-		L1_current_size = d1["max"] - d1["min"]
-		lowers = {1: d1["min"], 2: d2["min"]}
-		uppers = {1: d1["max"], 2: d2["max"]}
-		lower1 = max(lowers[1], math.ceil(lowers[2] / 2))
-		upper1 = min(uppers[1], math.floor(uppers[2] / 2))
-		if upper1 > d1["max"] or lower1 < d1["min"] or L1_new_size < 0:
-			return CONTRADICTION
-		elif L1_new_size == L1_current_size:
-			return DOMAIN_INTACT
-		elif L1_new_size < L1_current_size:
-			new_domain = {"min": lower1, "max": upper1}
-			return new_domain
-			
-	def __new_L2_domain(self, d1, d2):
-		'''Determines new bounds for L2 W.R.T. l1_half_l2 constraint.
-		
-		If new consistent bounds exists, it returns the new legal domain.
-		This is a mathematical function.'''
-		L2_current_size = d2["max"] - d2["min"]
-		lowers = {1: d1["min"], 2: d2["min"]}
-		uppers = {1: d1["max"], 2: d2["max"]}		
-		lower2 = max(lowers[2], lowers[1] * 2)
-		upper2 = min(uppers[2], uppers[1] * 2)
-		if upper2 > d2["max"] or lower2 < d2["min"] or L2_new_size < 0:
-			return CONTRADICTION
-		elif L2_new_size == L2_current_size:
-			return DOMAIN_INTACT
-		elif L2_new_size < L2_current_size:
-			new_domain = {"min": lower2, "max": upper2}
-			return new_domain
-		
-	def __L2_may_reduce(self, reduced_vars, A):
-		'''Decides whether the domain of L2 may reduce or not.
-		
-		This is a mathematical function.'''
-		return "L1" in reduced_vars and not "L2" in A
-	
-	def __L1_may_reduce(self, reduced_vars, A):
-		'''Decides whether the domain of L1 may reduce or not.
-		
-		This is a mathematical function.'''
-		return "L2" in reduced_vars and not "L1" in A
-	
-	def b_update(self, reduced_vars):
-		'''Establishes indirect consistency W.R.T. l1_half_l2 constraint.
-		
-		If L1 is in the assignment, L2 is already made consistency
-		in the establish function; hence, no further reduction would occur.
+        There must be this relation between assigned values to L1 and L2:
 
-		These boundary updates happen if the domain of L1 is reduced:		
-
-			lower2 = max(lower2, lower1*2)
-			upper2 = min(upper2, upper1*2)
-			
-		and these updates happen if the domain of L2 is reduced:
+        L1 = L2 / 2     or      L2 = L1 * 2
 		
-			lower1 = max(lower1, lower2/2) 
-			upper1 = min(upper1, upper2/2)
-		
-		Note: only one of the above updates happen. In case of
-		contradiction, both reduced variables are involved. That's why
-		reduced_vars is returned as the conflict set.
+		Also the boundaries could be made consistent.
 		'''
-		A = self.__csp.get_assignment()
-		D = self.__csp.get_domains()
-		impacted = set([])
-		res = DOMAI_INTACT
-		if self.__L2_can_adjust(reduced_vars, A):
-			new_L2_domain = self.__new_L2_domain(D["L1"], D["L2"])
-			if new_L2_domain == CONTRADICTION:
-				return (CONTRADICTION, reduced_vars, "l1_half_l2")
-			elif new_L2_domain != DOMAIN_INTACT:
-				impacted.add("L2")
-				self.__csp.update_d("L2", new_L2_domain)
-		elif self.__L1_can_adjust(reduced_vars, A):
-			new_L1_domain = self.__new_L1_domain(D["L1"], D["L2"])
-			if new_L1_domain == CONTRADICTION:
-				return (CONTRADICTION, reduced_vars, "l1_half_l2")
-			elif new_L1_domain != DOMAIN_INTACT:		
-				impacted.add("L1")
-				self.__csp.update_d("L1", new_L1_domain)
-		if res == DOMAIN_INTACT:
-			return (DOMAINS_INTACT, set([]))
-		return (DOMAINS_REDUCED, impacted)
 	
-	def __has_impact(self, curvar):
-		'''Checkes whether establishing consistency is possible.
-		
-		Upon assigning L1, there is an opportunity to reduce the domain of 
-		L2.
-		This is a mathematical function.'''
-		return curvar == "L1"
-		
-	def establish(self, curvar, value):
-		'''Establishes direct consistency W.R.T. l1_half_l2 constraint.
+	def establish(self, csp, curvar, value):
+		'''Establishes consistency W.R.T. l1_half_l2 constraint.
 		
 		The domain of L2 reduces to L1 * 2.
 	
 		If L1 is assigned a value, L2 reduces to one value only. However,
 		the domain of L2 will still be represented via bounds, now with
-		equal min & max.'''
-		if not self.__has_impact(curvar):
+		equal min & max.
+		
+		If the assigned variable is L2, nothing happens. The assumption is
+		that L1 is always assigned first.
+
+		L1 = 100
+		L2 = {"min": 80,  "max":90}
+		'''
+		if curvar != "L1":
 			return (DOMAINS_INTACT, set([]))
-		d1 = {"min": value, "max": value}
-		D = self.__csp.get_domains()
-		new_L2_domain = self.__new_L2_domain(d1, D["L2"])
-		if new_L2_domain == CONTRADICTION:
-			return (CONTRADICTION, {"L1"}, "l1_half_l2")
-		elif new_L2_domain == DOMAIN_INTACT:
-			return (DOMAINS_INTACT, set([]))
-		self.__csp.update_d("L2", new_L2_domain)
-		return (DOMAINS_REDUCED, {"L2"})
+		L2 = csp.get_domain("L2")
+		if L2["min"] > value * 2 or L2["max"] < value * 2:
+			return (CONTRADICTION, {"L2"})
+		if L2["max"] == L2["max"] and value * 2 == L2["max"]:
+			return (DOMAINS_INTACT, {"L2"})
+		csp.update_domain("L2", {"min": value * 2, "max": value * 2})
+		return (DOMAINS_REDUCED, {"L2"}, {"L2"})
+
+	def propagate(self, csp, reduced_vars):
+		'''Establishes bounds consistency W.R.T. l1_half_l2 constraint.
+		
+		Note: only one of the above updates happen. In case of
+		contradiction, both reduced variables are involved. That's why
+		reduced_vars is returned as the conflict set.
+		'''
+		L1 = csp.get_domain("L1")
+		L2 = csp.get_domain("L2")
+		if self.__consistent(L1, L2):
+			return (DOMAINS_INTACT, {"L1", "L2"})
+		elif self.__contradiction(L1, L2):
+				return (CONTRADICTION, {"L1", "L2"})
+		reduced = set([])
+		if L1["min"] * 2 > L2["min"] + 1: # min of L2 can adjust
+			csp.update_domain("L2", {"min": L1["min"] * 2, "max": L2["max"]})
+			reduced.add("L2")
+		elif L1["min"] * 2 < L2["min"] + 1: # min of L1 can adjust
+			new_min = math.ceil(L2["min"] / 2)
+			csp.update_domain("L1", {"min": new_min, "max": L1["max"]})
+			reduced.add("L1")
+		if L1["max"] * 2 > L2["max"] + 1: # max of L1 can adjust
+			new_max = math.ceil(L2["max"] / 2)
+			csp.update_domain("L1", {"min": L1["min"], "max": new_max})
+			reduced.add("L1")
+		elif L1["max"] * 2 < L2["max"] + 1: # max of L2 can adjust
+			csp.update_domain("L2", {"min": L2["min"], "max": L1["max"] * 2})
+			reduced.add("L2")
+		return (DOMAINS_REDUCED, {"L1", "L2"}, reduced)
+
+	def __contradiction(self, L1, L2):
+		'''Checkes if the domains are inconsistent but cannot adjust.
+		
+		A reduction that reduces one of the domains to one value is likely
+		causing a contradiction--if the other domain does not match.
+		'''
+		if L1["min"] == L1["max"] or L2["min"] == L2["max"]:
+			return True
+		return False
+
+	def __consistent(self, L1, L2):
+		'''Checks if the domains are already consistent.'''
+		if L1["min"] * 2 == L2["min"]:
+			if L1["max"] * 2 == L2["max"]:
+				return True
+		return False

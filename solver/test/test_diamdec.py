b2a57e3b37a2a824008b75a0cb64305b3d60131d
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-05 20:28:55 +0330

    Changes the inputs of test cases to make reading them simpler; adds docstrings to the test class

--- solver/test/test_diamdec.py
+++ solver/test/test_diamdec.py
@@ -11,31 +11,34 @@ from spec import specs
 from constants import *
 
 class test_DIAMDEC(unittest.TestCase):
-    '''The goal is to enforce the following constraint relations:
+    '''Test the behavior of Diameter decrement constraint.
+
+        The constraint makes sure the following relations exist between D
+        variables:
     
-        A:
+        0.5 <= D2 - D1 <= 1.0
+        0.5 <= D3 - D2 <= 1.0
+        0.5 <= D4 - D3 <= 1.0
+        0.5 <= D5 - D4 <= 1.0
+        0.5 <= D6 - D5 <= 1.0
+        0.5 <= D7 - D6 <= 1.0
 
-        0.5 <= D2 - D1 <= 1
-        0.5 <= D3 - D2 <= 1
-        0.5 <= D4 - D3 <= 1
-        0.5 <= D5 - D4 <= 1
-        0.5 <= D6 - D5 <= 1
-        0.5 <= D7 - D6 <= 1
+        The module does one or a combination of following actions:
 
-        i.e. the diamater difference between adjacent nodes must 
-        fall into an accepted range.
+        - returns without any action
+        - examines or does not examined variables
+        - reduces or does not reduce the domain of variables
+        - detects contradiction if consistency is impossible
 
-        Partitions:
+        Contradiction occurs if one varialbe shed all its values.
+        The module has two entry points: propagate and establish methods.
 
-        a. Di - D_i-1 = 0.5                         within range
-        b. Di - D_i-1 = 1                           within range
-        c. 0.5 < Di - D_i-1 < 1                     within range
-        d. Di - D_i-1 < 0.5                         out of range
-        e. Di - D_i-1 > 1                           out of range
-        f. Some values in D1 are illegal            removal from D1
-        h. No variable shed values                  DOMAINS_INTACT
-        i. Some variables shed values               DOMAINS_REDUCED
-        j. One variable shed all its values         CONTRADICTION'''
+        propagate method is not implemeted for now; hence, only
+        establish method is called.
+        
+        In the doc section of test cases, diff means the difference between 
+        a value of one D variable and a value in the one before it, for
+        example D2 - D1, D3 - D2, etc.'''
     
     def setUp(self):
         self.__csp = CSP()
@@ -44,13 +47,80 @@ class test_DIAMDEC(unittest.TestCase):
     def __reset_csp(self):
         self.__csp.unassign_all()
 
-    def test_contradiction_occurs(self):
+    def test_no_reduction(self):
+        '''Asserts a case that no reduction occurs.
+                
+            All values in the domain of all unassigned variables have one of
+            these values to be considered consistent:
+
+            diff = 1.0          a boundary value
+            diff = 0.5          a boundary value
+            0.5 < diff < 1.0    between the boundaries'''
+        # arrange
+        self.__reset_csp()
+        csp = self.__csp
+        csp.update_domain("D2", {13})
+        csp.update_domain("D3", {12.5})
+        csp.update_domain("D4", {11.5})
+        csp.update_domain("D5", {10.5})
+        csp.update_domain("D6", {9.5})
+        csp.update_domain("D7", {8.5, 8.6, 9})
+        # act
+        csp.assign("D1", 14)
+        output = self.__sut.establish(csp, "D1", 14)
+        # assess
+        self.assertEqual(output[0], DOMAINS_INTACT)
+        self.assertEqual(output[1], {"D2","D3","D4","D5","D6","D7"})
+
+    def test_reduction(self):
+        '''Asserts a case in which reduction occurs.
+                    
+            A value must have one of these values to be considered illegal:
+
+            diff < 0.5
+            diff > 1.0'''
+        # arrange
+        self.__reset_csp()
+        csp = self.__csp
+        csp.update_domain("D2", {13, 12})   # 12 is inconsistent
+        csp.update_domain("D3", {12.5, 13}) # 13 is inconsistent
+        csp.update_domain("D4", {11.5})
+        csp.update_domain("D5", {10.5})
+        csp.update_domain("D6", {9.5})
+        csp.update_domain("D7", {8.4, 8.5, 8.6, 9}) # 8.4 is inconsistent
+        # act
+        csp.assign("D1", 14)
+        output = self.__sut.establish(csp, "D1", 14)
+        # assess
+        self.assertEqual(output[0], DOMAINS_REDUCED)
+        self.assertEqual(output[1], {"D2","D3","D4","D5","D6","D7"})
+        self.assertEqual(output[2], {"D2","D3","D7"})
+        D = csp.get_domains()
+        self.assertEqual(D["D2"], {13})     # 12 is removed
+        self.assertEqual(D["D3"], {12.5})   # 12 is removed
+        self.assertEqual(D["D7"], {8.5, 9, 9.1}) # 8.4 is removed
+
+    def test_reduction_2(self):
+        '''Asserts another case in which reduction happens.'''
+
+        # arrange
+        self.__reset_csp()
+        csp = self.__csp
+        csp.update_domain("D7", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
+        # act
+        csp.assign("D6", 10)
+        output = self.__sut.establish(csp, "D6", 10)
+        # assess
+        self.assertEqual(output[0], DOMAINS_REDUCED)
+        self.assertEqual(output[1], {"D7"})
+        self.assertEqual(output[1], {"D7"})
+        D = csp.get_domains()
+        self.assertEqual(D["D7"], {9, 9.5})
+        
+    def test_contradiction(self):
         '''Asserts a contradictory case.
                     
-            One of these criteria must hold true for a value to be illegal:
-
-            Di - D_i-1 < 0.5
-            Di - D_i-1 > 1'''
+           All values of one unassigned variables are removed.'''
         # arrange
         self.__reset_csp()
         csp = self.__csp
@@ -61,90 +131,3 @@ class test_DIAMDEC(unittest.TestCase):
         # assess
         self.assertEqual(output[0], CONTRADICTION)
         self.assertEqual(output[1], {"D2"})
-
-    def test_reduction_occurs(self):
-        '''Asserts a case in which reduction happens.
-                    
-            One of these criteria must hold true for a value to be illegal:
-
-            Di - D_i-1 < 0.5
-            Di - D_i-1 > 1
-            
-            This case also covers the partition that no legal values in D2
-            is found for a value in D1.
-
-            For which, consistency is achieved by removing the value from D1.'''
-        # arrange
-        self.__reset_csp()
-        csp = self.__csp
-        csp.update_domain("D2", {13, 13.5, 13.6, 14})
-        csp.update_domain("D3", {11, 12, 12.5, 13, 13.5})
-        csp.update_domain("D4", {11, 11.5, 12, 12.5, 13})
-        csp.update_domain("D5", {10, 10.5, 11, 11.5, 12, 12.5})
-        csp.update_domain("D6", {8, 9, 9.5, 10, 10.5, 11, 11.5, 12})
-        csp.update_domain("D7", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        # act
-        csp.assign("D1", 14)
-        output = self.__sut.establish(csp, "D1", 14)
-        # assess
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"D2","D3","D4","D5","D6","D7"})
-        self.assertEqual(output[1], {"D2","D3","D4","D5","D6","D7"})
-        D = csp.get_domains()
-        self.assertEqual(D["D2"], {13, 13.5})
-        self.assertEqual(D["D3"], {12, 12.5, 13})
-        self.assertEqual(D["D4"], {11, 11.5, 12, 12.5})
-        self.assertEqual(D["D5"], {10, 10.5, 11, 11.5, 12})
-        self.assertEqual(D["D6"], {9, 9.5, 10, 10.5, 11, 11.5})
-        self.assertEqual(D["D7"], {8, 8.5, 9, 9.5, 10, 10.5, 11})
-
-    def test_reduction_occurs_2(self):
-        '''Asserts a case in which reduction happens.
-                    
-            One of these criteria must hold true for a value to be illegal:
-
-            Di - D_i-1 < 0.5
-            Di - D_i-1 > 1
-            
-            This case also covers the partition that no legal values in D2
-            is found for a value in D1.
-
-            For which, consistency is achieved by removing the value from D1.'''
-        # arrange
-        self.__reset_csp()
-        csp = self.__csp
-        csp.update_domain("D7", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        # act
-        csp.assign("D1", 14)
-        output = self.__sut.establish(csp, "D6", 10)
-        # assess
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"D7"})
-        self.assertEqual(output[1], {"D7"})
-        D = csp.get_domains()
-        self.assertEqual(D["D7"], {9, 9.5})
-
-    def test_no_reduction(self):
-        '''Asserts a case that no reduction occurs.
-                
-            One of these criteria must hold true for a value to be legal:
-
-            Di - D_i-1 = 0.5
-            Di - D_i-1 = 1
-            0.5 < Di - D_i-1 < 1'''
-        # arrange
-        self.__reset_csp()
-        csp = self.__csp
-        csp.update_domain("D2", {13, 13.5})
-        csp.update_domain("D3", {12, 12.5, 13})
-        csp.update_domain("D4", {11, 11.5, 12, 12.5})
-        csp.update_domain("D5", {10, 10.5, 11, 11.5, 12})
-        csp.update_domain("D6", {9, 9.5, 10, 10.5, 11, 11.5})
-        csp.update_domain("D7", {8, 8.5, 9, 9.5, 10, 10.5, 11})
-        # act
-        csp.assign("D1", 14)
-        output = self.__sut.establish(csp, "D1", 14)
-        # assess
-        self.assertEqual(output[0], DOMAINS_INTACT)
-        self.assertEqual(output[1], {"D2","D3","D4","D5","D6","D7"})
-        D = csp.get_domains()

60d3c0c12ef300bf2a3576d7666eb2fa772872a9
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-06 05:02:50 +0330

    Converts constraints hole2, hole4, and hole5 into unary constraints

--- solver/unary.py
+++ solver/unary.py
@@ -9,6 +9,8 @@ class UNARY():
 		self.__establish_holes_space(csp, spec)
 		self.__establish_RTD(csp, catalog, X)
 		self.__establish_D1(csp, spec)
+		self.__establish_L4(csp, spec)
+		self.__establish_L5(csp, spec)
 
 	def __establish_L(self, csp, spec, X):
 		'''Sets infinite positive integer values for all L variables.'''
@@ -59,3 +61,36 @@ class UNARY():
 			if diam >= spec["topd"]["min"] and diam <= spec["topd"]["max"]:
 				legal_values.add(diam)
 		csp.update_domain("D1", legal_values)
+
+	def __establish_L4(self, csp, spec):
+		'''Implements a length constraint on L4.
+			
+			The goal is to ensure holes 1 and 2 fall on node 4.
+
+			The following relation helps achieve this goal:
+			
+			L4 >= 2*hole_margin + 2*hole_diameter + h2 - h1 - hole_diameter
+			
+			which defines a minimum length for L4.'''
+		h2_h1_dist = spec["h2"] - spec["h1"]
+		new_min = 2*spec["hmarg"] + spec["holed"] + h2_h1_dist
+		# TODO: Should we check for contradiction here or not?
+		L4 = csp.get_domain("L4")
+		csp.update_domain("L4", {"min": new_min, "max": L4["max"]})
+
+	def __establish_L5(self, csp, spec):
+		'''Implements a length constraint on L5.
+			
+			The goal is to ensure holes 3, 4, and 5 falls on node 5.
+
+			The following relation helps achieve this goal:
+			
+			L5 >= two distances from top and bottom + distance between holes
+			3 & 5 + 3 hole diameters
+			
+			which defines a minimum length for L5.'''
+		h3_h5_dist = spec["h5"] - spec["h3"]
+		new_min = 2*spec["hmarg"] + spec["holed"] + h3_h5_dist
+		# TODO: Should we check for contradiction here or not?
+		L5 = csp.get_domain("L5")
+		csp.update_domain("L5", {"min": new_min, "max": L5["max"]})

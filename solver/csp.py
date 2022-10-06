a9c1f9eccc52b9044e6289d19f1722c21c55724a
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-06 18:20:56 +0330

    Removes extra constraits from csp

--- solver/csp.py
+++ solver/csp.py
@@ -3,36 +3,33 @@ import copy
 class CSP():
 
 	def __init__(self):
-		self.__X = {	"L1", "L2", "L3", "L4", "L5", "L6", "L7",
+		self.__X = { "L1", "L2", "L3", "L4", "L5", "L6", "L7",
 					"D1", "D2", "D3", "D4", "D5", "D6", "D7",
 					"R1", "R2", "R3", "R4", "R5", "R6", "R7",
 					"T1", "T2", "T3", "T4", "T5", "T6", "T7"}
 		self.__C = {
-			"same_t":	 {"T1", "T2", "T3", "T4", "T5", "T6", "T7"},
-			"same_r":  {"R1", "R2", "R3", "R4", "R5", "R6", "R7"},
-			"d_dec":	 {"D1", "D2", "D3", "D4", "D5", "D6", "D7"},
-			"stock1":  {"T1", "R1", "D1"},
-			"stock2":  {"T2", "R2", "D2"},
-			"stock3":  {"T3", "R3", "D3"},
-			"stock4":  {"T4", "R4", "D4"},
-			"stock5":  {"T5", "R5", "D5"},
-			"stock6":  {"T6", "R6", "D6"},
-			"stock7":  {"T7", "R7", "D7"},
-			"half":	 {"L1", "L2"},
-			"l_dec":   {"L2", "L3", "L4", "L5", "L6", "L7"},
-			"h1":	 {"L1", "L2", "L3"},
-			"h2":	 {"L1", "L2", "L3"},
-			"h3":	 {"L1", "L2", "L3", "L4"},
-			"h4":	 {"L1", "L2", "L3", "L4"},
-			"h5":	 {"L1", "L2", "L3", "L4"},
-			"h6":	 {"L1", "L2", "L3", "L4", "L5"},
-			"len":	 {"L1", "L2", "L3", "L4", "L5", "L6", "L7"}
+			"samethick":{"T1", "T2", "T3", "T4", "T5", "T6", "T7"},
+			"sameround":{"R1", "R2", "R3", "R4", "R5", "R6", "R7"},
+			"diamdec":	{"D1", "D2", "D3", "D4", "D5", "D6", "D7"},
+			"len":	 	{"L1", "L2", "L3", "L4", "L5", "L6", "L7"},
+			"lendec":  	{"L2", "L3", "L4", "L5", "L6", "L7"},
+			"hole6":	{"L1", "L2", "L3", "L4", "L5"},
+			"hole3":	{"L1", "L2", "L3", "L4"},
+			"stock1":	{"T1", "R1", "D1"},
+			"stock2":	{"T2", "R2", "D2"},
+			"stock3":	{"T3", "R3", "D3"},
+			"stock4":	{"T4", "R4", "D4"},
+			"stock5":	{"T5", "R5", "D5"},
+			"stock6":	{"T6", "R6", "D6"},
+			"stock7":	{"T7", "R7", "D7"},
+			"hole1":	{"L1", "L2", "L3"},
+			"half":		{"L1", "L2"},
 		}
 		self.__D = {}
-		self.__domains_backup = [] # order matters
+		self.__domains_backups = [] # order matters
 		self.__assignment = {}
 		self.__unassigned = copy.deepcopy(self.__X)
-		self.__assigned = [] # order matters		
+		self.__assigned = [] # order matters
 	
 	def update_domain(self, var, new_domain):
 		self.__D[var] = new_domain
@@ -46,6 +43,9 @@ class CSP():
 	def get_assignment(self):
 		return self.__assignment
 	
+	def get_unassigned_vars(self):
+		return self.__unassigned
+		
 	def assigned_count(self):
 		return len(self.__assigned)
 	

ccb42751aa5219a59aaa0c2774fbe5ae8d306672
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-07 07:49:10 +0330

    Adds test cases for lendec; refactors it and makes the tests pass

--- solver/lendec.py
+++ solver/lendec.py
@@ -3,96 +3,23 @@ from constants import *
 import math
 
 class LENDEC():
-	'''Applies length decrement consistency.
+	'''Implements length decrement consistency.
 	
 	The following relations must hold between L2 through L7:
 	
-		L2 > L3 > 2/3 L2
-		L3 > L4 > 2/3 L3
-		L4 > L5 > 2/3 L4
-		L5 > L6 > 2/3 L5
-		L6 > L7
-	
-	TODO: Move the following descriptions into the paper.
-
-	This consistency function is called on two occasions:
-		1- An L variable is assigned a value
-		2- Domain of an L variable has been reduced
-		
-		In case 1, we use the assigned value to determine a new 
-		consistent range for further variables. For example, if L3 
-		is assigned, say, 100, upper bound on L4, L5, L6 reduce to 
-		99, 98, 97, and 96 respectively. Furthermore, the lower bounds
-		of L4, L5, and L6 reduce to around 66, 44, and 29 respectively.
-		i.e. {L3: 100} propagates to
-		
-			L4 -> [66, 99] 
-			L5 -> [44, 98]
-			L6 -> [29, 96]
-		
-		In case 2, we use new bounds on the variable whose domain has
-		been reduced. That variable is curvar. For example, if the 
-		domain of L3 has reduced to, say, [80, 120], the domain of 
-		L4, L5, and L6 change as such:
-		
-			L4 -> [53, 119]
-			L5 -> [35, 118]
-			L6 -> [23, 117]
-		
-		From the two examples above, we can observe that assigning
-		values to variables can trigger stronger domain reduction
-		in propagation.
-
-		The assumption is that assignments occur in this order: L1, L2, L3,
-		L4, L5, L6, and then L7.'''
+	L2 > L3 > 2/3 L2
+	L3 > L4 > 2/3 L3
+	L4 > L5 > 2/3 L4
+	L5 > L6 > 2/3 L5
+	L6 > L7'''
 	
 	def establish(self, csp, curvar, value):
-		'''Establishes consistency W.R.T. len decrement constraint.
-	
-		upper3 = min(upper3, L2 - 1)
-		upper4 = min(upper4, upper3 - 1)
-		upper5 = min(upper5, upper4 - 1)
-		upper6 = min(upper6, upper5 - 1)
-		upper7 = min(upper7, upper6 - 1)
-		
-		lower3 = max(lower3, 2/3 * L2)
-		lower4 = max(lower4, 2/3 lower3)
-		lower5 = max(lower5, 2/3 lower4)
-		lower6 = max(lower6, 2/3 lower5)'''
+		'''Establishes consistency after curvar: value assignment.'''
 		if curvar == "L7":
 			return (DOMAINS_INTACT, set([]))
-		return self.__establish(csp, curvar)
-
-	def propagate(self, csp, reduced_vars):
-		'''Establishes indirect consistency W.R.T. l_dec constraint.
-		
-		if, say, L4, L5, and L6 are reduced, establishing consistency
-		just for L4 makes consistency of L5 and L6 fall into place.'''
-		reduced_sorted = sorted(reduced_vars)
-		start_var = reduced_sorted[0]
-		return self.__establish(csp, start_var)
-
-	def __establish(self, csp, start_var):
-		'''Establishes both indirect and direct l_dec consistency.
-	
-		lower3 = max(lower3, 2/3 lower2)
-		upper3 = min(upper3, upper2 - 1)
-
-		lower4 = max(lower4, 2/3 lower3)
-		upper4 = min(upper4, upper3 - 1)
-
-		lower5 = max(lower5, 2/3 lower4)
-		upper5 = min(upper5, upper4 - 1)
-
-		lower6 = max(lower6, 2/3 lower5)
-		upper6 = min(upper6, upper5 - 1)
-		
-		upper7 = min(upper7, upper6 - 1)
-		
-		This is a mutable shell function.'''
 		A = csp.get_assignment()
 		D = csp.get_domains()
-		(newbounds, examined) = self.__newbounds(A, D, start_var)
+		(newbounds, examined) = self.__newbounds(A, D, curvar)
 		if newbounds == CONTRADICTION:
 			return (CONTRADICTION, examined)
 		for Li, new_domain in newbounds.items():
@@ -102,14 +29,22 @@ class LENDEC():
 			return (DOMAINS_INTACT, examined)
 		return (DOMAINS_REDUCED, examined, reduced)
 
-	def __getprevs(self, A, D, start_var):
-		if start_var in A:
-			lprev = A[start_var]
-			uprev = A[start_var]
-		else:
-			lprev = D[start_var]["min"]
-			uprev = D[start_var]["max"]
-		return (lprev, uprev)
+	def propagate(self, csp, reduced_vars):
+		'''Establishes consistency after reduction of some variables.'''
+		reduced_sorted = sorted(reduced_vars)
+		if reduced_sorted[0] == "L7":
+			return (DOMAINS_INTACT, set([]))
+		A = csp.get_assignment()
+		D = csp.get_domains()
+		(newbounds, examined) = self.__newbounds(A, D, reduced_sorted[0])
+		if newbounds == CONTRADICTION:
+			return (CONTRADICTION, examined)
+		for Li, new_domain in newbounds.items():
+			csp.update_domain(Li, new_domain)
+		reduced = set(newbounds.keys())
+		if len(reduced) == 0:
+			return (DOMAINS_INTACT, examined)
+		return (DOMAINS_REDUCED, examined, reduced)
 	
 	def __in_range(self, range, value):
 		return value >= range["min"] and value <= range["max"]
@@ -118,26 +53,32 @@ class LENDEC():
 		return upper - lower < domain["max"] - domain["min"]
 
 	def __newbounds(self, A, D, start_var):
-		'''Returns new consistent bounds W.R.T. length decrement.
+		'''Calculates new consistent bounds.
 	
 		This is a mathematical function.'''
-		(lprev, uprev) = self.__getprevs(A, D, start_var)
 		newbounds = {}
 		examined = set([])
 		for i in range(int(start_var[1]) + 1, 8):
+			lprev = "L" + str(i-1)
+			if lprev in newbounds:
+				Dprev = {"min": lower, "max": upper}
+			elif lprev in A:
+				Dprev = {"min": A[lprev], "max": A[lprev]}
+			else:
+				Dprev = {"min": D[lprev]["min"], "max": D[lprev]["max"]}
 			li = "L" + str(i)
+			if li in A:
+				continue
 			examined.add(li)
 			if i < 7:
-				lower = math.ceil(2/3 * lprev)
+				lower = math.ceil(2/3 * Dprev["min"])
 			else:
-				lower = min(D[li]["min"], lprev - 1)
-			upper = uprev - 1
+				lower = Dprev["min"] - 1
+			upper = Dprev["max"] - 1
 			if not self.__in_range(D[li], lower):
 				return (CONTRADICTION, examined)
 			if not self.__in_range(D[li], upper):
 				return (CONTRADICTION, examined)
 			if self.__reduced(upper, lower, D[li]):
 				newbounds[li] = {"min": lower, "max": upper}
-			lprev = lower
-			uprev = upper
 		return (newbounds, examined)
--- solver/test/test_lendec.py
+++ solver/test/test_lendec.py
@@ -11,43 +11,7 @@ from spec import specs
 from constants import *
 
 class test_LENDEC(unittest.TestCase):
-    '''The goal is to enforce the following constraint relations:
-    
-		L2 > L3 >= 2/3 L2
-		L3 > L4 >= 2/3 L3
-		L4 > L5 >= 2/3 L4
-		L5 > L6 >= 2/3 L5
-		L6 > L7
-        
-        Length of nodes decrease from top to bottom (except node 1). The
-        amount of this decrement however must fall within a range.
-        
-        Boundary value analysis
-
-        Illegal values:
-
-        L3[min] < 2/3 A_L2         .
-        L3[min] < 2/3 L2[min]      .
-        L3[max] >= A_L2             .
-        L3[max] >= L2[max]          .
-        A_L6 <= L7[max]             .
-        L6[max] <= L7[max]          .
-
-        Legal values:
-
-        L2 > L3 >= 2/3 L2   legal   .
-        L6 > L7             legal   
-
-        Contradiction cases:
-        
-        L3[max] < 2/3 L2[min]
-        L3[max] < 2/3 A_L2
-        L3[min] >= L2[min]
-        L3[min] >= A_L2
-        L7[min] >= L6[max]
-        L7[min] >= A_L6
-
-        '''
+    '''Tests the behavior of len decrement consistency.'''
     
     def setUp(self):
         self.__csp = CSP()
@@ -57,18 +21,16 @@ class test_LENDEC(unittest.TestCase):
         self.__csp.unassign_all()
 
     def test_propagate_reduces_bounds(self):
-        '''A case propagate detects and removes inconsistent values.
-        
-        In this case L2 changes are propagatedand L5 has legal boundaries'''
+        '''A case in which propagate detects and removes inconsistent values.'''
         # arrange
         self.__reset_csp()
         csp = self.__csp
         csp.update_domain("L4", {"min": 90, "max": 80})
         csp.update_domain("L5", {"min": 59, "max": 80})     # to 60, 79
         csp.update_domain("L6", {"min": 39, "max": 79})     # to 40, 78
-        csp.update_domain("L7", {"min": 30, "max": 78})     # to 30, 77
+        csp.update_domain("L7", {"min": 39, "max": 78})     # to 39, 77
         # act
-        out = self.__sut.propagate(csp, {"L4"})
+        out = self.__sut.propagate(csp, {"L4", "L6", "L7"})
         # assess
         D = csp.get_domains()
         self.assertEqual(out[0], DOMAINS_REDUCED)
@@ -76,23 +38,10 @@ class test_LENDEC(unittest.TestCase):
         self.assertEqual(out[2], {"L5", "L6", "L7"})
         self.assertEqual(D["L5"], {"min": 60, "max": 79})
         self.assertEqual(D["L6"], {"min": 40, "max": 78})
-        self.assertEqual(D["L7"], {"min": 30, "max": 77})
+        self.assertEqual(D["L7"], {"min": 39, "max": 77})
 
     def test_establish_reduces_bounds(self):
-        '''A case that all inconsistency conditions exist and are fixed.
-        
-        In this case L2 is assigned and L5 has legal boundaries
-
-        L3[min] < 2/3 A_L2         L3[min] must increase
-        L3[max] >= A_L2            L3[max] must decrease
-        L4[min] < 2/3 L3[min]      L4[min] must increase
-        L4[max] >= L3[max]         L4[max] must decrease
-        L5[min] >= 2/3 L4[max]     L5 remains intact
-        L5[max] < L4[min]          L5 remains intact
-        L6[min] >= 2/3 L5[max]     L6 remains intact
-        L6[max] < L5[min]          L6 remains intact
-        L6[max] <= L7[max]         L7[max] must decrease
-        '''
+        '''A case that all inconsistency conditions exist and are fixed.'''
         # arrange
         self.__reset_csp()
         csp = self.__csp
@@ -112,10 +61,32 @@ class test_LENDEC(unittest.TestCase):
         self.assertEqual(D["L3"], {"min": 54, "max": 80})
         self.assertEqual(D["L4"], {"min": 36, "max": 79})
         self.assertEqual(D["L6"], {"min": 16, "max": 77})
-        self.assertEqual(D["L7"], {"min": 13, "max": 76})
+        self.assertEqual(D["L7"], {"min": 15, "max": 76})
+
+    def test_establish_reduces_bounds_2(self):
+        '''Another case in which inconsisten values are removed.'''
+        # arrange
+        self.__reset_csp()
+        csp = self.__csp
+        csp.assign("L2", 81)
+        csp.update_domain("L3", {"min": 53, "max": 81})    # to 54, 80
+        csp.assign("L4", 36)
+        csp.update_domain("L5", {"min": 23, "max": 36})    # to 24, 35
+        csp.assign("L6", 18)
+        csp.update_domain("L7", {"min": 16, "max": 18})    # to 17, 17
+        # act
+        out = self.__sut.establish(csp, "L2", 81)
+        # assess
+        D = csp.get_domains()
+        self.assertEqual(out[0], DOMAINS_REDUCED)
+        self.assertEqual(out[1], {"L3", "L5", "L7"})
+        self.assertEqual(out[2], {"L3", "L5", "L7"})
+        self.assertEqual(D["L3"], {"min": 54, "max": 80})
+        self.assertEqual(D["L5"], {"min": 24, "max": 35})
+        self.assertEqual(D["L7"], {"min": 17, "max": 17})
 
     def test_contradiction_occurs(self):
-        '''A case that contradiction occurs.'''
+        '''A case in which contradiction occurs.'''
         # arrange
         self.__reset_csp()
         csp = self.__csp
@@ -128,7 +99,7 @@ class test_LENDEC(unittest.TestCase):
         self.assertEqual(out[1], {"L3"})
 
     def test_contradiction_occurs_2(self):
-        '''A case that contradiction occurs.'''
+        '''Another case in which contradiction occurs.'''
         # arrange
         self.__reset_csp()
         csp = self.__csp

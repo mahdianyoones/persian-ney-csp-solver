00fddcf6b290a439397b46b24f160c0d88d25f6f
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-18 17:17:49 +0330

    Redesigns lendec with insights from ac3

--- solver/lendec.py
+++ solver/lendec.py
@@ -1,95 +1,125 @@
-from functools import reduce
 from constants import *
-import math
+import copy
 
 class LENDEC():
     '''Implements length decrement consistency.
     
-    The following relations must hold between L2 through L7:
+    Consecutive nodes must decrease in length. The following relations must
+    hold between L2 through L7:
     
-    L2 > L3 > 2/3 L2
-    L3 > L4 > 2/3 L3
-    L4 > L5 > 2/3 L4
-    L5 > L6 > 2/3 L5
+    L2 > L3
+
+    L3 > L4
+
+    L4 > L5
+
+    L5 > L6
+
     L6 > L7'''
-    
+    def __init__(self):
+        self.__neighbors = {("L2", "L3"), ("L3", "L4"),
+                ("L4", "L5"), ("L5", "L6"), ("L6", "L7")}
+
     def establish(self, csp, curvar, value):
         '''Establishes consistency after curvar: value assignment.'''
-        if curvar == "L7":
-            return (DOMAINS_INTACT, set([]))
         A = csp.get_assignment()
+        queue = set([])
+        i = int(curvar[1])
+        c1 = ("L"+str(i), "L"+str(i+1))
+        c2 = ("L"+str(i-1), "L"+str(i))
+        if c1 in self.__neighbors:
+            queue.add(c1)
+        if c2 in self.__neighbors:
+            queue.add(c2)
         D = csp.get_domains()
-        (newbounds, examined) = self.__newbounds(A, D, curvar)
-        if newbounds == CONTRADICTION:
-            confset = self.__confset(csp)
-            return (CONTRADICTION, examined, confset)
-        for Li, new_domain in newbounds.items():
-            csp.update_domain(Li, new_domain)
-        reduced = set(newbounds.keys())
-        if len(reduced) == 0:
-            return (DOMAINS_INTACT, examined)
-        return (DOMAINS_REDUCED, examined, reduced)
+        return self.__ac3(csp, A, D, queue)
 
     def propagate(self, csp, reduced_vars):
         '''Establishes consistency after reduction of some variables.'''
-        reduced_sorted = sorted(reduced_vars)
-        if reduced_sorted[0] == "L7":
-            return (DOMAINS_INTACT, set([]))
         A = csp.get_assignment()
+        queue = set([])
+        for rvar in reduced_vars:
+            i = int(rvar[1])
+            c1 = ("L"+str(i), "L"+str(i+1))
+            c2 = ("L"+str(i-1), "L"+str(i))
+            if c1 in self.__neighbors:
+                queue.add(c1)
+            if c2 in self.__neighbors:
+                queue.add(c2)        
         D = csp.get_domains()
-        (newbounds, examined) = self.__newbounds(A, D, reduced_sorted[0])
-        if newbounds == CONTRADICTION:
-            confset = self.__confset(csp)
-            return (CONTRADICTION, examined, confset)
-        for Li, new_domain in newbounds.items():
-            csp.update_domain(Li, new_domain)
-        reduced = set(newbounds.keys())
-        if len(reduced) == 0:
-            return (DOMAINS_INTACT, examined)
-        return (DOMAINS_REDUCED, examined, reduced)
-    
-    def __in_range(self, range, value):
-        return value >= range["min"] and value <= range["max"]
-    
-    def __reduced(self, upper, lower, domain):
-        return upper - lower < domain["max"] - domain["min"]
+        return self.__ac3(csp, A, D, queue)
 
-    def __confset(self, csp):
-        '''Returns the conflict set.'''
-        confset = set([])
-        A = csp.get_assignment()
-        for v in {"L2", "L3", "L4", "L5", "L6", "L7"}:
-            if v in A:
-                confset.add(v)
-        return confset
-
-    def __newbounds(self, A, D, start_var):
-        '''Calculates new consistent bounds.
-    
-        This is a mathematical function.'''
-        newbounds = {}
+    def __ac3(self, csp, A, D, queue):
         examined = set([])
-        for i in range(int(start_var[1]) + 1, 8):
-            lprev = "L" + str(i-1)
-            if lprev in newbounds:
-                Dprev = {"min": lower, "max": upper}
-            elif lprev in A:
-                Dprev = {"min": A[lprev], "max": A[lprev]}
-            else:
-                Dprev = {"min": D[lprev]["min"], "max": D[lprev]["max"]}
-            li = "L" + str(i)
-            if li in A:
+        confset = set([])
+        reduced = set([])
+        while len(queue) > 0:
+            (Li, Lj) = queue.pop()
+            if Li in A and Lj in A:
+                confset.update({Li, Lj})
                 continue
-            examined.add(li)
-            if i < 7:
-                lower = math.ceil(2/3 * Dprev["min"])
+            if Li in A:
+                confset.add(Li)
             else:
-                lower = Dprev["min"] - 1
-            upper = Dprev["max"] - 1
-            if not self.__in_range(D[li], lower):
-                return (CONTRADICTION, examined)
-            if not self.__in_range(D[li], upper):
-                return (CONTRADICTION, examined)
-            if self.__reduced(upper, lower, D[li]):
-                newbounds[li] = {"min": lower, "max": upper}
-        return (newbounds, examined)
+                examined.add(Li)
+            if Lj in A:
+                confset.add(Lj)
+            else:
+                examined.add(Lj)
+            (Di, Dj, new_pairs) = self.__revise(Li, Lj, A, D)
+            if Di == CONTRADICTION or Dj == CONTRADICTION:
+                return (CONTRADICTION, examined, confset)
+            if Di != DOMAIN_INTACT:
+                reduced.add(Li)
+                csp.update_domain(Li, Di)
+            elif Dj != DOMAIN_INTACT:
+                reduced.add(Lj)
+                csp.update_domain(Lj, Dj)
+            if len(new_pairs) > 0:
+                queue.update(new_pairs)
+        if len(reduced) > 0:
+            return (DOMAINS_REDUCED, examined, confset)
+        return (DOMAINS_INTACT, examined)
+
+    def __revise(self, Li, Lj, A, D):
+        '''Canculates new consistent bounds for Li and Lj where Lj < Li.'''
+        new_pairs = set([])
+        Di = DOMAIN_INTACT
+        Dj = DOMAIN_INTACT
+        if not Li in A:
+            if Lj in A:
+                new_min = A[Lj] + 1
+            else:
+                new_min = D[Lj]["min"] + 1
+            Di = {"min": new_min, "max": D[Li]["max"]}
+        if not Lj in A:
+            if Li in A:
+                new_max = A[Li] - 1
+            else:
+                new_max = D[Li]["max"] - 1
+            Dj = {"min": D[Lj]["min"], "max": new_max}
+        if Di != DOMAIN_INTACT:
+            if Di["min"] > Di["max"]:
+                Di = CONTRADICTION
+            elif Di["min"] <= D[Li]["min"]:
+                Di = DOMAIN_INTACT
+            else: # reduced
+                new_pairs.update(self.__new_pairs(int(Li[1])))
+        if Dj != DOMAIN_INTACT:
+            if Dj["max"] < Dj["min"]:
+                Dj = CONTRADICTION
+            elif Dj["max"] >= D[Lj]["max"]:
+                Dj = DOMAIN_INTACT
+            else: # reduced
+                new_pairs.update(self.__new_pairs(int(Lj[1])))
+        return (Di, Dj, new_pairs)
+
+    def __new_pairs(self, i):
+        pair1 = ("L"+str(i), "L"+str(i+1))
+        pair2 = ("L"+str(i-1), "L"+str(i))
+        new_pairs = set([])
+        if pair1 in self.__neighbors:
+            new_pairs.add(pair1)
+        if pair2 in self.__neighbors:
+            new_pairs.add(pair2)
+        return new_pairs

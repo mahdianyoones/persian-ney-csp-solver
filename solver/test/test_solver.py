024956db076d076f50eabe71a840e21876a981bb
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-24 14:39:57 +0330

    Modifies the solver test cases to check existense and non-existense of solutins for all kooks

--- solver/test/test_solver.py
+++ solver/test/test_solver.py
@@ -19,25 +19,27 @@ from constants import *
 class test_SOLVER(unittest.TestCase):
 
     def test_finds_a_solution(self):
-        # arrange
-        catalog = CATALOG(current+"/contains_solutions.csv")
-        csp = CSP()
-        select = SELECT(csp)
-        mac = MAC(csp, catalog, specs["C"])
-        sut = SOLVER(csp, select, mac)
-        # act
-        res = sut.find(catalog, specs["C"])
-        # assess
-        self.assertEqual(res[0], SOLUTION)
+        for kook in {"F_tall", "G", "A", "Bb", "C", "D", "E", "F_short"}:
+            # arrange
+            catalog = CATALOG(current+"/contains_solutions.csv")
+            csp = CSP()
+            select = SELECT(csp)
+            mac = MAC(csp, catalog, specs[kook])
+            sut = SOLVER(csp, select, mac)
+            # act
+            res = sut.find(catalog, specs[kook])
+            # assess
+            self.assertEqual(res[0], SOLUTION)
 
     def test_finds_no_solution(self):
-        # arrange
-        catalog = CATALOG(current+"/contains_no_solution.csv")
-        csp = CSP()
-        select = SELECT(csp)
-        mac = MAC(csp, catalog, specs["C"])
-        sut = SOLVER(csp, select, mac)
-        # act
-        res = sut.find(catalog, specs["C"])
-        # assess
-        self.assertNotEqual(res[0], SOLUTION)
+        for kook in {"F_tall", "G", "A", "Bb", "C", "D", "E", "F_short"}:
+            # arrange
+            catalog = CATALOG(current+"/contains_no_solution.csv")
+            csp = CSP()
+            select = SELECT(csp)
+            mac = MAC(csp, catalog, specs[kook])
+            sut = SOLVER(csp, select, mac)
+            # act
+            res = sut.find(catalog, specs[kook])
+            # assess
+            self.assertNotEqual(res[0], SOLUTION)

fdd251253868514dd6d0485a690ec37de6853adf
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-27 15:57:21 +0330

    Fixes an issue with select class

--- solver/pickup.py
+++ solver/pickup.py
@@ -71,12 +71,12 @@ class SELECT():
 	def nextval(self, curvar, domain):
 		'''Returns the next value in the domain of curvar.'''		
 		if curvar[0] == "L":
-			if domain["min"] > domain["max"]:
-				return DOMAIN_EXHAUSTED
-			elif curvar == "L2":
+			if curvar == "L2":
 				domain["min"] += 2
 			else:
 				domain["min"] += 1
+			if domain["min"] > domain["max"]:
+				return DOMAIN_EXHAUSTED
 			return domain["min"]
 		elif len(domain) == 0: # D, T, and R variables
 			return DOMAIN_EXHAUSTED

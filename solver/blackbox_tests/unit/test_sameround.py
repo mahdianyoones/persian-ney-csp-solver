639c116ab4f092c56ee8685f53b0c172d773820c
Yoones Mahdian <MahdianYoones@gmail.com>
2022-10-30 22:34:53 +0330

    Refactors same_round unit tests

--- solver/blackbox_tests/unit/test_sameround.py
+++ solver/blackbox_tests/unit/test_sameround.py
@@ -10,59 +10,61 @@ from sameround import SAMEROUND
 from constants import *
 
 class test_SAMEROUND(unittest.TestCase):
-    '''Enfroces same roundness constraint to R variables.
-
-    Partitions:
-
-    a. R1 is not being assigned
-    b. At least one of R2, R3, ... to R7 does not contain the value of R1
-    c. All variables R2, R3, ... to R7 do contain the value of R1'''
+    '''Tests the behavior of sameround constraint.'''
 
     def setUp(self):
         self.__csp = CSP()
         self.__sut = SAMEROUND()
     
-    def test_contradiction_occurs(self):
-        '''R1 = some_value, some_value does not exist in at least one domain.
-        
-        R1 = 0
-        0 does not exist in R2'''
+    def test_establishes_and_reduces(self):
+        # arrange
         csp = self.__csp
-        csp.update_domain("R1", {0})
-        csp.update_domain("R2", {2.5})
-        csp.update_domain("R3", {0, 2.5})
-        csp.update_domain("R4", {0, 2.5})
-        csp.update_domain("R5", {0, 2.5})
-        csp.update_domain("R6", {0, 2.5})
-        csp.update_domain("R7", {0, 2.5})
+        domains = {
+            "R1": {2.5, 2},
+            "R2": {2.5, 5, 4},
+            "R3": {0, 2.5},
+            "R4": {0, 2.5},
+            "R5": {0, 2.5},
+            "R6": {0, 2.5},
+            "R7": {0, 2.5}
+        }
+        for var, vals in domains.items():
+            csp.update_domain(var, vals)
+        # act
+        output = self.__sut.establish(csp, "R2", 2.5)
+        # assess
+        self.assertEqual(output[0], DOMAINS_REDUCED)
+        self.assertEqual(output[1], {"R1", "R3", "R4", "R5", "R6", "R7"})
+        self.assertEqual(output[2], {"R1", "R3", "R4", "R5", "R6", "R7"})
+        D = csp.get_domains()
+        self.assertEqual(D["R2"], {2.5, 5, 4})
+        for var in {"R1", "R3", "R4", "R5", "R6", "R7"}:
+            self.assertEqual(D[var], {2.5})
+
+    def test_contradiction_occurs(self):
+        # arrange
+        csp = self.__csp
+        domains = {
+            "R1": {0},
+            "R2": {2.5},
+            "R3": {0, 2.5},
+            "R4": {0, 2.5},
+            "R5": {0, 2.5},
+            "R6": {0, 2.5},
+            "R7": {0, 2.5}
+        }
+        for var, vals in domains.items():
+            csp.update_domain(var, vals)
+        # act
         output = self.__sut.establish(csp, "R1", 0)
+        # assess
         self.assertEqual(output[0], CONTRADICTION)
     
-    def test_domains_reduce(self):
-        '''R1 = some value, some value exists in all domains as well.
-
-        R1 = 2.5
-        2.5 exists in all variables'''
-        csp = self.__csp
-        csp.update_domain("R1", {2.5})
-        csp.update_domain("R2", {2.5})
-        csp.update_domain("R3", {0, 2.5})
-        csp.update_domain("R4", {0, 2.5})
-        csp.update_domain("R5", {0, 2.5})
-        csp.update_domain("R6", {0, 2.5})
-        csp.update_domain("R7", {0, 2.5})
-        output = self.__sut.establish(csp, "R1", 2.5)
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"R2", "R3", "R4", "R5", "R6", "R7"})
-        D = csp.get_domains()
-        for _var in {"R2", "R3", "R4", "R5", "R6", "R7"}:
-            self.assertEqual(D[_var], {2.5})
-
-    def test_no_impact(self):
+    def test_establishes_once_only(self):
         '''Enforces the case that the algorithm simply does nothing!
         
-        The algorithm only checks domains if R1 IS BEING established.
-        Otherwise, we are sure that domains are consistent AND further
+        The algorithm only checks domains if THE FIRST R variable is being
+        established. Otherwise, we are sure that domains are consistent AND further
         reduction is impossible.
         
         The domains remain intact in this case, and they are NOT examined.'''
@@ -71,7 +73,7 @@ class test_SAMEROUND(unittest.TestCase):
         output = self.__sut.establish(csp, "R2", 2.5)
         self.assertEqual(output, (DOMAINS_INTACT, set([])))
 
-    def test_no_reduction(self):
+    def test_all_contain_the_same_value(self):
         '''Enforces a case that domains are checked but no value is removed.
         
         i.e. the domains happend to be just consistent.

9b0dd5553f0096b4aa300e5d374f293b8426ddf8
Yoones Mahdian <MahdianYoones@gmail.com>
2022-11-01 14:25:20 +0330

    Refactors lendec_lower

--- solver/blackbox_tests/unit/test_lendec_lower.py
+++ solver/blackbox_tests/unit/test_lendec_lower.py
@@ -8,71 +8,75 @@ sp.append(grandparent)
 from csp import CSP
 from lendec_lower import LENDEC_LOWER
 from constants import *
+import case_runner
 
 class test_LENDEC_LOWER(unittest.TestCase):
-    '''Tests the behavior of len decrement consistency.
-    
-    The following inequalities are established between variables L2 to L6:
-
-    L3 >= 2/3 L2
-
-    L4 >= 2/3 L3
-
-    L5 >= 2/3 L4
-
-    L6 >= 2/3 L5'''
+    '''Tests the behavior of len decrement constraint.'''
     
     def setUp(self):
         self.__csp = CSP()
         self.__sut = LENDEC_LOWER()
+        self.__case_runner = case_runner.test_CASE_RUNNER()
 
-    def test_establish_reduces_bounds(self):
-        '''A case in which propagate detects and removes inconsistent values.'''
-        # arrange
+    def test_domains_are_reduced(self):
+        sut = self.__sut
         csp = self.__csp
-        csp.assign("L2", 121)
-        csp.assign("L3", 90)
-        csp.update_domain("L4", {"min": 59, "max": 89})     # to 60, 89
-        csp.update_domain("L5", {"min": 39, "max": 88})     # to 40, 88
-        csp.update_domain("L6", {"min": 35, "max": 87})
-        # act
-        out = self.__sut.establish(csp, "L3", 90)
-        # assess
-        D = csp.get_domains()
-        self.assertEqual(out[0], DOMAINS_REDUCED)
-        self.assertEqual(out[2], {"L4", "L5"})
-        self.assertEqual(D["L4"], {"min": 60, "max": 89})
-        self.assertEqual(D["L5"], {"min": 40, "max": 88})
+        assert_constraint = self.__case_runner.assert_constraint
+        # case 1
+        given = {
+            "A": {"L2": 121, "L3": 90},
+            "D": {
+                "L4": {"min": 59, "max": 89},
+                "L5": {"min": 39, "max": 88},
+                "L6": {"min": 35, "max": 87}
+            },
+            "curvar": "L3",
+            "value": 90
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED, {"L4", "L5", "L6"}, {"L4", "L5"}),
+            "D": {
+                "L4": {"min": 60, "max": 89},
+                "L5": {"min": 40, "max": 88}
+            }
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
+        # case 2
+        given = {
+            "D": {
+                "L2": {"min": 90, "max": 172},
+                "L3": {"min": 89, "max": 171},
+                "L4": {"min": 88, "max": 170},
+                "L5": {"min": 87, "max": 169},
+                "L6": {"min": 30, "max": 248}
+            },
+            "reduced_vars": {"L2","L3","L4","L5","L6"},
+            "value": 90
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED, {"L2","L3","L4","L5","L6"}, {"L6"}),
+            "D": {
+                "L6": {"min": 58, "max": 248}
+            }
+        }
+        assert_constraint(csp, sut, "propagate", given, expect)
 
-    def test_bug1212(self):
-        '''A case in which propagate detects and removes inconsistent values.'''
-        # arrange
+    def test_contradiction_is_detected(self):
+        sut = self.__sut
         csp = self.__csp
-        csp.update_domain("L2", {"min": 90, "max": 172}) # examined, intact
-        csp.update_domain("L3", {"min": 89, "max": 171}) # examined, intact
-        csp.update_domain("L4", {"min": 88, "max": 170}) # examined, intact
-        csp.update_domain("L5", {"min": 87, "max": 169}) # examined, intact
-        csp.update_domain("L6", {"min": 30, "max": 248}) # reduced
-        # act
-        out = self.__sut.propagate(csp, {"L2","L3","L4","L5","L6"})
-        # assess
-        D = csp.get_domains()
-        self.assertEqual(out[0], DOMAINS_REDUCED)
-        self.assertEqual(out[1], {"L2","L3","L4","L5","L6"})
-        self.assertEqual(out[2], {"L6"})
-        self.assertEqual(D["L6"], {"min": 58, "max": 248})
-
-    def test_contradiction_occurs(self):
-        '''A case in which contradiction occurs.'''
-        # arrange
-        csp = self.__csp
-        csp.assign("L2", 90)
-        csp.update_domain("L3", {"min": 1, "max": 59}) # min cannot become 60
-       # act
-        out = self.__sut.establish(csp, "L2", 90)
-        # assess
-        self.assertEqual(out[0], CONTRADICTION)
-        self.assertEqual(out[2], {"L2"})
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "A": {"L2": 90},
+            "D": {
+                "L3": {"min": 1, "max": 59},
+            },
+            "curvar": "L2",
+            "value": 90
+        }
+        expect = {
+            "out": (CONTRADICTION, {"L3"}, {"L2"}),
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()

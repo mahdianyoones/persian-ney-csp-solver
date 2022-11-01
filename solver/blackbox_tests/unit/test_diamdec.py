0d3438552ec7373b2bbb31a4d6f9bea9b9303232
Yoones Mahdian <MahdianYoones@gmail.com>
2022-11-01 13:32:39 +0330

    Refactors test_diamdec

--- solver/blackbox_tests/unit/test_diamdec.py
+++ solver/blackbox_tests/unit/test_diamdec.py
@@ -8,6 +8,7 @@ sp.append(grandparent)
 from csp import CSP
 from diamdec import DIAMDEC
 from constants import *
+import case_runner
 
 class test_DIAMDEC(unittest.TestCase):
     '''Test the behavior of diameter decrement constraint.'''
@@ -15,179 +16,206 @@ class test_DIAMDEC(unittest.TestCase):
     def setUp(self):
         self.__csp = CSP()
         self.__sut = DIAMDEC({"min": 0.5, "max": 1.0})
+        self.__case_runner = case_runner.test_CASE_RUNNER()
     
-    def test_no_reduction_after_establish(self):
-        '''Asserts a case that no reduction occurs.'''
-        # arrange
+    def test_establish_leaves_all_intact(self):
+        sut = self.__sut
         csp = self.__csp
-        csp.update_domain("D2", {13})
-        # act
-        csp.assign("D1", 14)
-        output = self.__sut.establish(csp, "D1", 14)
-        # assess
-        self.assertEqual(output[0], DOMAINS_INTACT)
-        self.assertEqual(output[1], {"D2"})
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "A": {"D1": 14},
+            "D": {
+                "D2": {13},
+            },
+            "curvar": "D1",
+            "value": 14
+        }
+        expect = {
+            "out": (DOMAINS_INTACT, {"D2"})
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
 
-    def test_no_reduction_after_propagation(self):
-        '''Asserts a case that no reduction occurs.'''
-        # arrange
+    def test_propagation_leaves_all_intact(self):
+        sut = self.__sut
         csp = self.__csp
-        csp.update_domain("D1", {14})
-        csp.update_domain("D2", {13})
-        csp.update_domain("D3", {12.5})
-        csp.update_domain("D4", {11.5})
-        csp.update_domain("D5", {10.5})
-        csp.update_domain("D6", {9.5})
-        csp.update_domain("D7", {8.5, 8.6, 9})
-        # act
-        csp.assign("D1", 14)
-        output = self.__sut.propagate(csp, {"D5", "D4", "D1", "D7"})
-        # assess
-        D = csp.get_domains()
-        self.assertEqual(output[0], DOMAINS_INTACT)
-        self.assertEqual(output[1], {"D2","D3","D4","D5","D6","D7"})
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "A": {"D1": 14},
+            "D": {
+                "D1": {14},
+                "D2": {13},
+                "D3": {12.5},
+                "D4": {11.5},
+                "D5": {10.5},
+                "D6": {9.5},
+                "D7": {8.5, 8.6, 9},
+            },
+            "reduced_vars": {"D5", "D4", "D1", "D7"},
+        }
+        expect = {
+            "out": (DOMAINS_INTACT, {"D2","D3","D4","D5","D6","D7"})
+        }
+        assert_constraint(csp, sut, "propagate", given, expect)
 
-    def test_reduction_after_propagation(self):
-        '''Asserts a case in which reduction occurs.'''
-        # arrange
+    def test_propagation_causes_reduction(self):
+        sut = self.__sut
         csp = self.__csp
-        csp.update_domain("D1", {14})
-        csp.update_domain("D2", {13, 12})   # 12 is inconsistent
-        csp.update_domain("D3", {12.5, 13}) # 13 is inconsistent
-        csp.update_domain("D4", {11.5, 12})
-        csp.update_domain("D5", {10.5, 11})
-        csp.update_domain("D6", {9.5, 10})
-        csp.update_domain("D7", {8.4, 8.5, 8.6, 9}) # 8.4 is inconsistent
-        # act
-        output = self.__sut.propagate(csp, {"D1", "D6"})
-        # assess
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"D1","D2","D3","D4","D5","D6","D7"})
-        self.assertEqual(output[2], {"D2","D3","D7"})
-        D = csp.get_domains()
-        self.assertEqual(D["D2"], {13})     # 12 is removed
-        self.assertEqual(D["D3"], {12.5})   # 12 is removed
-        self.assertEqual(D["D7"], {8.5, 8.6, 9}) # 8.4 is removed
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "D": {
+                "D1": {14},
+                "D2": {13, 16},
+                "D3": {12.5, 13},
+                "D4": {11.5, 12},
+                "D5": {10.5, 11},
+                "D6": {9.5, 10},
+                "D7": {8.4, 8.5, 8.6, 9},
+            },
+            "reduced_vars": {"D1", "D6"},
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED,
+                {"D1","D2","D3","D4","D5","D6","D7"},
+                {"D2","D3","D7"}),
+            "D": {
+                "D2": {13},
+                "D3": {12.5},
+                "D7": {8.5, 8.6, 9}
+            }
+        }
+        assert_constraint(csp, sut, "propagate", given, expect)
+        given = {
+            "D": {
+                "D1": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D2": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D3": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D4": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D5": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D6": {7, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D7": {6},
+            },
+            "reduced_vars": {"D7"},
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED,
+                {"D1","D2","D3","D4","D5","D6","D7"},
+                {"D1", "D2", "D3", "D4", "D5", "D6"}),
+            "D": {
+                "D1": {10, 10.5, 11, 11.5},
+                "D2": {9.5, 10, 10.5, 11},
+                "D3": {9, 9.5, 10},
+                "D4": {8.5, 9},
+                "D5": {8},
+                "D6": {7},
+                "D7": {6},
+            }
+        }
+        assert_constraint(csp, sut, "propagate", given, expect)
 
-    def test_reduction_after_establish(self):
-        '''Asserts another case in which reduction happens.'''
-        # arrange
-        csp = self.__csp
-        csp.update_domain("D7", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        # act
-        csp.assign("D5", 11)
-        csp.assign("D6", 10)
-        output = self.__sut.establish(csp, "D6", 10)
-        # assess
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"D7"})
-        self.assertEqual(output[2], {"D7"})
-        D = csp.get_domains()
-        self.assertEqual(D["D7"], {9, 9.5})
-    
     def test_reduction_by_establish(self):
-        # arrange
+        sut = self.__sut
         csp = self.__csp
-        csp.update_domain("D1", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D2", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D3", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D4", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D5", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D6", {7, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D7", {6, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        # act
-        csp.assign("D1", 10.5)
-        output = self.__sut.establish(csp, "D1", 10.5)
-        # assess
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"D2", "D3", "D4", "D5", "D6", "D7"})
-        self.assertEqual(output[2], {"D2", "D3", "D4", "D5", "D6", "D7"})
-        D = csp.get_domains()
-        self.assertEqual(D["D2"], {9.5, 10})
-        self.assertEqual(D["D3"], {9, 9.5})
-        self.assertEqual(D["D4"], {8.5, 9})
-        self.assertEqual(D["D5"], {8})
-        self.assertEqual(D["D6"], {7})
-        self.assertEqual(D["D7"], {6})
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "A": {"D5": 11, "D6": 10},
+            "D": {
+                "D7": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+            },
+            "curvar": "D6",
+            "value": 10
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED, {"D7"}, {"D7"}),
+            "D": {
+                "D7": {9, 9.5}
+            }
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
+        given = {
+            "A": {"D1": 10.5},
+            "D": {
+                "D1": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D2": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D3": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D4": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D5": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D6": {7, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D7": {6, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+            },
+            "curvar": "D1",
+            "value": 10.5
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED, 
+            {"D2", "D3", "D4", "D5", "D6", "D7"}, 
+            {"D2", "D3", "D4", "D5", "D6", "D7"}),
+            "D": {
+                "D2": {9.5, 10},
+                "D3": {9, 9.5},
+                "D4": {8.5, 9},
+                "D5": {8},
+                "D6": {7},
+                "D7": {6},
+            }
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
+        given = {
+            "A": {"D7": 6},
+            "D": {
+                "D1": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D2": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D3": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D4": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D5": {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D6": {7, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+                "D7": {6, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13},
+            },
+            "curvar": "D7",
+            "value": 6
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED, 
+            {"D1", "D2", "D3", "D4", "D5", "D6"},
+            {"D1", "D2", "D3", "D4", "D5", "D6"}),
+            "D": {
+                "D1": {10, 10.5, 11, 11.5},
+                "D2": {9.5, 10, 10.5, 11},
+                "D3": {9, 9.5, 10},
+                "D4": {8.5, 9},
+                "D5": {8},
+                "D6": {7}
+            }
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
 
-    def test_reduction_by_establish_2(self):
-        # arrange
+    def test_establish_detects_contradiction(self):
+        sut = self.__sut
         csp = self.__csp
-        csp.update_domain("D1", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D2", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D3", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D4", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D5", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D6", {7, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D7", {6, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        # act
-        csp.assign("D7", 6)
-        output = self.__sut.establish(csp, "D7", 6)
-        # assess
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"D1", "D2", "D3", "D4", "D5", "D6"})
-        self.assertEqual(output[2], {"D1", "D2", "D3", "D4", "D5", "D6"})
-        D = csp.get_domains()
-        self.assertEqual(D["D1"], {10, 10.5, 11, 11.5})
-        self.assertEqual(D["D2"], {9.5, 10, 10.5, 11})
-        self.assertEqual(D["D3"], {9, 9.5, 10})
-        self.assertEqual(D["D4"], {8.5, 9})
-        self.assertEqual(D["D5"], {8})
-        self.assertEqual(D["D6"], {7})
-
-    def test_reduction_by_propagate(self):
-        # arrange
-        csp = self.__csp
-        csp.update_domain("D1", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D2", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D3", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D4", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D5", {5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D6", {7, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 13})
-        csp.update_domain("D7", {6})
-        # act
-        output = self.__sut.propagate(csp, {"D7"})
-        # assess
-        self.assertEqual(output[0], DOMAINS_REDUCED)
-        self.assertEqual(output[1], {"D1", "D2", "D3", "D4", "D5", "D6", "D7"})
-        self.assertEqual(output[2], {"D1", "D2", "D3", "D4", "D5", "D6"})
-        D = csp.get_domains()
-        self.assertEqual(D["D1"], {10, 10.5, 11, 11.5})
-        self.assertEqual(D["D2"], {9.5, 10, 10.5, 11})
-        self.assertEqual(D["D3"], {9, 9.5, 10})
-        self.assertEqual(D["D4"], {8.5, 9})
-        self.assertEqual(D["D5"], {8})
-        self.assertEqual(D["D6"], {7})
-        self.assertEqual(D["D7"], {6})
-
-    def test_contradiction_after_establish(self):
-        '''Asserts a contradictory case.'''
-        # arrange
-        csp = self.__csp
-        csp.update_domain("D2", {13, 13.5, 13.6, 14})
-        # act
-        csp.assign("D1", 12.5)
-        output = self.__sut.establish(csp, "D1", 12.5)
-        # assess
-        self.assertEqual(output[0], CONTRADICTION) # output indicator
-        self.assertEqual(output[1], {"D2"}) # examined set
-        self.assertEqual(output[2], {"D1"}) # conflict set
-
-    def test_returns_correct_conflict_set(self):
-        '''Asserts a correct conflcit set is returned.'''
-        # arrange
-        csp = self.__csp
-        csp.update_domain("D5", {13, 13.5, 13.6, 14})
-        # act
-        csp.assign("D1", 14)
-        csp.assign("D2", 13)
-        csp.assign("D3", 12)
-        csp.assign("D4", 11)
-        output = self.__sut.establish(csp, "D4", 11)
-        # assess
-        self.assertEqual(output[0], CONTRADICTION) # output indicator
-        self.assertEqual(output[1], {"D5"}) # examined set
-        self.assertEqual(output[2], {"D1", "D2", "D3", "D4"}) # conflict set
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "A": {"D1": 12.5},
+            "D": {
+                "D2": {13, 13.5, 13.6, 14},
+            },
+            "curvar": "D1",
+            "value": 12.5
+        }
+        expect = {
+            "out": (CONTRADICTION, {"D2"}, {"D1"}),
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
+        given = {
+            "A": {"D1": 14, "D2": 13, "D3": 12, "D4": 11},
+            "D": {
+                "D5": {13, 13.5, 13.6, 14},
+            },
+            "curvar": "D4",
+            "value": 11
+        }
+        expect = {
+            "out": (CONTRADICTION, {"D5"}, {"D1", "D2", "D3", "D4"}),
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()

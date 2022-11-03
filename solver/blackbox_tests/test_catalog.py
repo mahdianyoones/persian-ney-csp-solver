1fddcc813db77b2c7727e455c17ecd4eb92c97d4
Yoones Mahdian <MahdianYoones@gmail.com>
2022-11-03 19:20:18 +0330

    Adds several test cases to test new changes in catalog; one of them fails

--- solver/blackbox_tests/test_catalog.py
+++ solver/blackbox_tests/test_catalog.py
@@ -13,22 +13,85 @@ class test_CATALOG(unittest.TestCase):
 
     def setUp(self):
         self.__sut = CATALOG()
+        self.__sut.setup(current+"/catalog.csv")
 
-    def test_correct_values_are_loaded_without_filers(self):
-        # arrange
-        self.__sut.setup(current+"/real_pieces.csv")
+    def test_returns_correct_thick_without_filters(self):
         # act
         T_vals = self.__sut.values("T")
-        R_vals = self.__sut.values("R")
-        D_vals = self.__sut.values("D")
-        l = self.__sut.l()
         # assess
-        self.assertEqual(T_vals, {1, 1.5, 2, 2.5, 3, 3.5})
-        self.assertEqual(R_vals, {0, 0.5, 1, 1.5, 2, 2.5})
-        expected_D_vals = {19, 18, 17.5, 21.5, 21, 20.5, 20, 19.5, 17, 16.5, 16,
-            15.5, 15, 14.5, 14, 13.5, 25, 23.5, 18.5, 24}
+        self.assertEqual(T_vals, {2, 2.5})
+
+    def test_returns_correct_thick_with_filters(self):
+        # act
+        T_vals = self.__sut.values("T", {"D": 13, "R": 0})
+        # assess
+        self.assertEqual(T_vals, {2.0, 2.5})
+        # act
+        T_vals = self.__sut.values("T", {"D": 13, "R": 1})
+        # assess
+        self.assertEqual(T_vals, {2})
+
+    def test_returns_correct_round_without_filters(self):
+        # act
+        R_vals = self.__sut.values("R")
+        # assess
+        self.assertEqual(R_vals, {0, 1})
+
+    def test_returns_correct_round_with_filters(self):
+        # act
+        R_vals = self.__sut.values("R", {"D": 12.4})
+        # assess
+        self.assertEqual(R_vals, {0})
+
+    def test_returns_correct_diamter_without_filters(self):
+        # act
+        D_vals = self.__sut.values("D")
+        # assess
+        expected_D_vals = {19, 18, 17, 16, 15, 14, 13, 12.4}
         self.assertEqual(D_vals, expected_D_vals)
-        self.assertEqual(l, 12051)
+
+    def test_returns_correct_diamter_with_filters(self):
+        # act
+        D_vals = self.__sut.values("D", {"T": 2, "R": 1})
+        # assess
+        expected_D_vals = {13}
+        self.assertEqual(D_vals, expected_D_vals)
+
+    def test_returns_correct_pieces_without_filters(self):
+        # act
+        pieces = self.__sut.pieces()
+        # assess
+        expected_pieces = {
+            ("1", 100), 
+            ("2", 200), 
+            ("3", 80),
+            ("4", 70), 
+            ("5", 150), 
+            ("6", 130), 
+            ("7", 60), 
+            ("8", 170), 
+            ("9", 220), 
+            ("10", 300), 
+            ("11", 120)
+        }
+        self.assertEqual(pieces, expected_pieces)
+
+    def test_returns_correct_pieces_with_filters(self):
+        # act
+        pieces = self.__sut.pieces({"T": 2, "R": 0})
+        # assess
+        expected_pieces = {
+            ("1", 100), 
+            ("2", 200), 
+            ("3", 80),
+            ("4", 70), 
+            ("5", 150), 
+            ("6", 130), 
+            ("7", 60), 
+            ("10", 300), 
+            ("11", 120)
+        }
+        self.assertEqual(pieces, expected_pieces)
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()

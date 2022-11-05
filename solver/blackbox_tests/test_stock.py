f5828f2c6c6898b20da2affc05039e58b937e96b
Yoones Mahdian <MahdianYoones@gmail.com>
2022-11-05 17:04:58 +0330

    Replaces stock test cases with new ones based on new design

--- solver/blackbox_tests/test_stock.py
+++ solver/blackbox_tests/test_stock.py
@@ -11,190 +11,82 @@ from csp import CSP
 from stock import STOCK
 from constants import *
 from catalog import CATALOG
+from unary import UNARY
 import case_runner
 
 class test_STOCK(unittest.TestCase):
-    '''Tests the behavior of stock constraints along with catalog.'''
+    '''Tests the behavior of stock constraints.
+    
+    Assumes catalog is well-tested.'''
     
     def setUp(self):
         self.__csp = CSP()
-        self.__catalog = CATALOG()
-        self.__catalog.setup()
-        pieces = [
-          # (T, D,  R, L)
-            [1, 18, 0, 10],
-            [1, 19, 0, 10],
-            [1, 19, 0, 10],
-            [2, 19, 1, 10],
-            [2, 19, 0, 10],
-            [3, 20, 2, 10]
-        ]
-        for piece in pieces:
-            self.__catalog.add_piece(piece[0], piece[1], piece[2], piece[3]) 
+        self.__catalog = CATALOG(current+"/stock.csv")
         self.__sut = STOCK(self.__catalog)
+        UNARY.init_domains(self.__csp, self.__catalog)
         self.__case_runner = case_runner.test_CASE_RUNNER()
 
-    def __get_initial_domains(self):
-        T_vals = self.__catalog.values("T")
-        R_vals = self.__catalog.values("R")
-        D_vals = self.__catalog.values("D")
-        l = self.__catalog.l()
-        return (T_vals, R_vals, D_vals, l)
+    def tests_R_is_assigned_PDT_get_reduced(self):
+        sut = self.__sut
+        csp = self.__csp
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "A": {"R1": 1},
+            "curvar": "R1",
+            "value": 1
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED, set([]), {"D1", "P1", "T1"}),
+            "D": {
+                "D1": {13},
+                "T1": {2},
+                "P1": {("8", 170)}
+            }
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
+        
+    def tests_RDT_are_assigned_P_gets_reduced(self):
+        sut = self.__sut
+        csp = self.__csp
+        assert_constraint = self.__case_runner.assert_constraint
+        given = {
+            "A": {"R1": 0, "T1": 2, "D1": 14},
+            "curvar": "R1",
+            "value": 0
+        }
+        expect = {
+            "out": (DOMAINS_REDUCED, set([]), {"P1"}),
+            "D": {
+                "P1": {("6", 130), ("7", 60)}
+            }
+        }
+        assert_constraint(csp, sut, "establish", given, expect)
 
     def tests_establish_detects_contradiction(self):
         sut = self.__sut
         csp = self.__csp
         assert_constraint = self.__case_runner.assert_constraint
-        (T_vals, R_vals, D_vals, l) = self.__get_initial_domains()
         given = {
             "A": {"D1": 18, "R1": 1},
-            "D": {
-                "T1": T_vals,
-                "L1": {"min": 1, "max": l}
-            },
             "curvar": "R1",
             "value": 1
         }
         expect = {
-            "out": (CONTRADICTION, set([]), {"D1", "R1"})
-        }
-        assert_constraint(csp, sut, "establish", given, expect)
-        given = {
-            "A": {"D1": 18, "T1": 2},
-            "D": {
-                "R1": R_vals,
-                "L1": {"min": 1, "max": l}
-            },
-            "curvar": "T1",
-            "value": 2
-        }
-        expect = {
-            "out": (CONTRADICTION, set([]), {"D1", "T1"})
-        }
-        assert_constraint(csp, sut, "establish", given, expect)
-        given = {
-            "A": {"T1": 1, "R1": 1},
-            "D": {
-                "D1": D_vals,
-                "L1": {"min": 1, "max": l}
-            },
-            "curvar": "R1",
-            "value": 1
-        }
-        expect = {
-            "out": (CONTRADICTION, set([]), {"T1", "R1"})
+            "out": (CONTRADICTION, set([]), {"D1", "R1", "P1"})
         }
         assert_constraint(csp, sut, "establish", given, expect)
     
-    def tests_finds_pieces(self):
-        sut = self.__sut
-        csp = self.__csp
-        assert_constraint = self.__case_runner.assert_constraint
-        (T_vals, R_vals, D_vals, l) = self.__get_initial_domains()
-        given = {
-            "A": {"T1": 1, "R1": 0, "D1": 19},
-            "D": {
-                "L1": {"min": 1, "max": l}
-            },
-            "curvar": "R1",
-            "value": 0
-        }
-        expect = {
-            "out": (DOMAINS_REDUCED, {"L1"}, {"L1"}),
-            "D": {
-                "L1": {"min": 1, "max": 20}
-            }
-        }
-        assert_constraint(csp, sut, "establish", given, expect)
-        given = {
-            "A": {"R1": 0, "D1": 19},
-            "D": {
-                "T1": T_vals,
-                "L1": {"min": 1, "max": l}
-            },
-            "curvar": "R1",
-            "value": 0
-        }
-        expect = {
-            "out": (DOMAINS_REDUCED, {"L1", "T1"}, {"L1", "T1"}),
-            "D": {
-                "L1": {"min": 1, "max": 30},
-                "T1": {1, 2}
-            }
-        }
-        assert_constraint(csp, sut, "establish", given, expect)
-        given = {
-            "A": {"T1": 2, "D1": 19},
-            "D": {
-                "R1": R_vals,
-                "L1": {"min": 1, "max": l}
-            },
-            "curvar": "T1",
-            "value": 2
-        }
-        expect = {
-            "out": (DOMAINS_REDUCED, {"L1", "R1"}, {"L1", "R1"}),
-            "D": {
-                "L1": {"min": 1, "max": 20},
-                "R1": {1, 0}
-            }
-        }
-        assert_constraint(csp, sut, "establish", given, expect)
-        given = {
-            "A": {"R1": 0},
-            "D": {
-                "T1": T_vals,
-                "D1": D_vals,
-                "L1": {"min": 1, "max": l}
-            },
-            "curvar": "R1",
-            "value": 0
-        }
-        expect = {
-            "out": (DOMAINS_REDUCED, {"L1", "T1", "D1"}, {"L1", "T1", "D1"}),
-            "D": {
-                "T1": {1, 2},
-                "D1": {18, 19},
-                "L1": {"min": 1, "max": 40}
-            }
-        }
-        assert_constraint(csp, sut, "establish", given, expect)
-        given = {
-            "A": {"R1": 0},
-            "D": {
-                "T1": T_vals,
-                "D1": D_vals,
-                "L1": {"min": 1, "max": l}
-            },
-            "curvar": "R1",
-            "value": 0
-        }
-        expect = {
-            "out": (DOMAINS_REDUCED, {"L1", "T1", "D1"}, {"L1", "T1", "D1"}),
-            "D": {
-                "T1": {1, 2},
-                "D1": {18, 19},
-                "L1": {"min": 1, "max": 40}
-            }
-        }
-        assert_constraint(csp, sut, "establish", given, expect)
-
-    def test_does_not_find_enough_pieces(self):
+    def tests_propagate_takes_no_action(self):
         sut = self.__sut
         csp = self.__csp
         assert_constraint = self.__case_runner.assert_constraint
         given = {
-            "A": {"D1": 19, "T1": 1, "R1": 0},
-            "D": {
-                "L1": {"min": 21, "max": 100}
-            },
-            "curvar": "T1",
-            "value": 1
+            "reduced_vars": {"R1"},
         }
         expect = {
-            "out": (CONTRADICTION, set([]), {"T1", "R1", "D1"}),
+            "out": (DOMAINS_INTACT, set([]))
         }
-        assert_constraint(csp, sut, "establish", given, expect)
+        assert_constraint(csp, sut, "propagate", given, expect)
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()

a6a6b47a97fe71825a2e820a3b12ff2a08996857
Yoones Mahdian <MahdianYoones@gmail.com>
2022-11-16 19:29:39 +0330

    Reimplements mac based on the new design

--- solver/mac.py
+++ solver/mac.py
@@ -1,3 +1,4 @@
+import copy
 from hole1 import HOLE1
 from hole3 import HOLE3
 from hole6 import HOLE6
@@ -8,114 +9,123 @@ from diamdec import DIAMDEC
 from samethick import SAMETHICK
 from sameround import SAMEROUND
 from half import HALF
-from stock import STOCK
+from stock import THICK_STOCK, DIAM_STOCK, PIECE_STOCK, ROUND_STOCK
+from piecemin import PIECEMIN
+from piecemax import PIECEMAX
 from constants import *
 
 class MAC():
-	'''Implements MAC through special consistency algorithms.'''
+    '''Implements MAC for binary and higher constraints.'''
 
-	def __init__(self, csp, catalog, spec):
-		self.__csp = csp
-		self.__X2C = {}
-		self.__constraints_order = {}
-		self.__init_c_order(csp)
-		self.__init_X2C(csp)
-		stock_ref = STOCK(catalog)
-		self.__refs = {
-			"hole1":		HOLE1(spec["h1"], spec["hmarg"]),
-			"hole3":		HOLE3(spec["h3"], spec["hmarg"]),
-			"hole6":		HOLE6(spec["h6"], spec["hmarg"]),
-			"half":			HALF(),
-			"stock1": 		stock_ref,
-			"stock2": 		stock_ref,
-			"stock3": 		stock_ref,
-			"stock4": 		stock_ref,
-			"stock5": 		stock_ref,
-			"stock6": 		stock_ref,
-			"stock7": 		stock_ref,
-			"samethick":	SAMETHICK(),
-			"sameround":	SAMEROUND(),
-			"lendec":		LENDEC(),
-			"lendec_lower":	LENDEC_LOWER(),
-			"diamdec":		DIAMDEC(spec["ddiff"]),
-			"len":			LEN(spec["len"])
-		}
+    def __init__(self, csp, catalog, spec):
+        self.__csp = csp
+        self.__catalog = catalog
+        self.__X2C = {}
+        self.__constraints_order = {}
+        self.__init_c_order(csp)
+        self.__init_X2C(csp)
+        self.__refs = {
+            "hole1":		HOLE1(spec["h1"], spec["hmarg"]),
+            "hole3":		HOLE3(spec["h3"], spec["hmarg"]),
+            "hole6":		HOLE6(spec["h6"], spec["hmarg"]),
+            "half":			HALF(),
+            "samethick":	SAMETHICK(),
+            "sameround":	SAMEROUND(),
+            "len":			LEN(spec["len"])
+        }
+        self.__pstockref = PIECE_STOCK(self.__catalog)
+        self.__rstockref = ROUND_STOCK(self.__catalog)
+        self.__dstockref = DIAM_STOCK(self.__catalog)
+        self.__tstockref = THICK_STOCK(self.__catalog)
+        self.__pieceminref = PIECEMIN()
+        self.__piecemaxref = PIECEMAX()
+        self.__diamdecref = DIAMDEC(spec["ddiff"])
+        self.__lendecref = LENDEC()
+        self.__llref = LENDEC_LOWER()
+        for i in range(1, 8):
+            self.__refs["pstock"+str(i)] = self.__pstockref
+            self.__refs["rstock"+str(i)] = self.__rstockref
+            self.__refs["dstock"+str(i)] = self.__dstockref
+            self.__refs["tstock"+str(i)] = self.__tstockref
+            self.__refs["piecemin"+str(i)] = self.__pieceminref
+            self.__refs["nodemax"+str(i)] = self.__piecemaxref
+            if i < 7:
+                self.__refs["diamdec"+str(i)] = self.__diamdecref
+            if i >= 2 and i < 7:
+                self.__refs["lendec"+str(i)] = self.__lendecref
+                self.__refs["lendeclower"+str(i)] = self.__llref
 
-	def establish(self, curvar, value):
-		'''Establishes consistency after var: value assignment.
-		
-		Calls all the consistency algorithms of the constraints on curvar, and
-		returns a conflict set in case of contradiction or reduced_vars, which
-		is a set of variables whose domains have been reduced.
+    def establish(self, curvar, value):
+        '''Establishes consistency after var: value assignment.
+        
+        Calls all the consistency algorithms of the constraints on curvar.
 
-		The effect of domain reductions is then kept in check by the
-		upper subroutine calling the propagate method.
-		
-		Constraints that may have bigger impacts are called first.'''
-		csp = self.__csp
-		constraints = self.__X2C[curvar]
-		reduced_vars = set([])
-		examined = set([])
-		evaled_consts = set([])
-		for constraint in constraints:
-			evaled_consts.add(constraint)
-			ref = self.__refs[constraint]
-			res = ref.establish(csp, curvar, value)
-			examined.update(res[1])
-			if res[0] == DOMAINS_REDUCED: 	
-				reduced_vars.update(res[2])
-			elif res[0] == CONTRADICTION:
-				return (CONTRADICTION, examined, res[2], evaled_consts)
-		if len(reduced_vars) > 0:
-			return (DOMAINS_REDUCED, examined, reduced_vars, evaled_consts)
-		return (DOMAINS_INTACT, examined, evaled_consts)
+        The effect of domain reductions is then kept in check by the
+        upper subroutine calling the propagate method.
+        
+        Constraints that may have bigger impacts are called first.'''
+        csp = self.__csp
+        constraints = self.__X2C[curvar]
+        reduced_vars = set([])
+        for const in constraints:
+            parts = csp.get_neighbors(const)
+            result = self.__refs[const].establish(csp, curvar, value, parts)
+            if result == CONTRADICTION:
+                return CONTRADICTION
+            elif isinstance(result, tuple) and result[0] == MADE_CONSISTENT:
+                reduced_vars.update(result[1])
+        if len(reduced_vars) > 0:
+            result = self.propagate(copy.deepcopy(reduced_vars))
+            if result == CONTRADICTION:
+                return CONTRADICTION
+            elif isinstance(result, tuple) and result[0] == MADE_CONSISTENT:
+                reduced_vars.update(result[1])
+        if len(reduced_vars) > 0:
+            return (MADE_CONSISTENT, reduced_vars)
+        return ALREADY_CONSISTENT
 
-	def propagate(self, reduced_vars):
-		'''Recursively propagates domain reductions.'''
-		csp = self.__csp
-		evaled_consts = set([])
-		new_reduced_vars = set([])
-		examined = set([])
-		while len(reduced_vars) > 0:
-			_var = reduced_vars.pop()
-			constraints = self.__X2C[_var]
-			for constraint in constraints:
-				evaled_consts.add(constraint)
-				participants = csp.get_neighbors(constraint)
-				reduced_prtcns = reduced_vars.intersection(participants)
-				reduced_prtcns.add(_var)
-				res = self.__refs[constraint].propagate(csp, reduced_prtcns)
-				examined.update(res[1])
-				if res[0] == CONTRADICTION:
-					return (CONTRADICTION, examined, res[2], evaled_consts)
-				if res[0] == DOMAINS_REDUCED:
-					new_reduced_vars.update(res[2])
-					reduced_vars.update(res[2])
-		if len(reduced_vars) > 0:
-			return (DOMAINS_REDUCED, examined, reduced_vars)
-		return (DOMAINS_INTACT, examined, evaled_consts)
-	
-	def __init_c_order(self, csp):
-		'''Determines the order of constraints.
-		
-		A constraint order denotes the number of participants it has.'''
-		constraints = csp.get_constraints()
-		for constraint, participants in constraints.items():
-			self.__constraints_order[constraint] = 8 - len(participants)
+    def propagate(self, reduced_vars):
+        '''Recursively propagates domain reductions.'''
+        csp = self.__csp
+        new_reduced_vars = set([])
+        while len(reduced_vars) > 0:
+            _var = reduced_vars.pop()
+            constraints = self.__X2C[_var]
+            for constraint in constraints:
+                participants = csp.get_neighbors(constraint)
+                reduced_prtcns = reduced_vars.intersection(participants)
+                reduced_prtcns.add(_var)
+                res = self.__refs[constraint].propagate(csp, reduced_prtcns, participants)
+                if res == CONTRADICTION:
+                    return CONTRADICTION
+                elif isinstance(res, tuple) and res[0] == MADE_CONSISTENT:
+                    new_reduced_vars.update(res[1])
+                    reduced_vars.update(res[1])
+        if len(new_reduced_vars) > 0:
+            return (MADE_CONSISTENT, new_reduced_vars)
+        return ALREADY_CONSISTENT
+    
+    def __init_c_order(self, csp):
+        '''Determines the order of constraints.
+        
+        A constraint order denotes the number of participants it has.'''
+        constraints = csp.get_constraints()
+        for constraint, participants in constraints.items():
+            self.__constraints_order[constraint] = 8 - len(participants)
 
-	def __init_X2C(self, csp):
-		'''Buils a map from each variable to the constraints they participate.
-		
-		Constraints of each variable are sorted based on their order.'''
-		C = csp.get_constraints()
-		X = csp.get_variables()
-		X2C = {}
-		corders = self.__constraints_order
-		for constraint, _vars in C.items():
-			for v in X:
-				if not v in X2C:
-					X2C[v] = set([])
-				if v in _vars:
-					X2C[v].add(constraint)
-		for v, constraints in X2C.items():
-			self.__X2C[v] = sorted(constraints, key=lambda c: corders[c])
+    def __init_X2C(self, csp):
+        '''Buils a map from each variable to the constraints they participate.
+        
+        Constraints of each variable are sorted based on their order.'''
+        C = csp.get_constraints()
+        X = csp.get_variables()
+        X2C = {}
+        corders = self.__constraints_order
+        for constraint, _vars in C.items():
+            for v in X:
+                if not v in X2C:
+                    X2C[v] = set([])
+                if v in _vars:
+                    X2C[v].add(constraint)
+        for v, constraints in X2C.items():
+            self.__X2C[v] = sorted(constraints, key=lambda c: corders[c])

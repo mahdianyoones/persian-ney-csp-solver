8bda40a9efcc3dbda295ca7a329021322c676c1e
Yoones Mahdian <MahdianYoones@gmail.com>
2022-11-16 19:37:19 +0330

    Removes old test cases of mac and adds a new one

--- solver/blackbox_tests/test_mac.py
+++ solver/blackbox_tests/test_mac.py
@@ -3,8 +3,11 @@ import os.path as op
 from sys import path as sp
 current = op.dirname(op.realpath(__file__))
 parent = op.dirname(current)
+grandparent = op.dirname(op.dirname(current))
 sp.append(parent)
+sp.append(grandparent)
 
+import copy
 from csp import CSP
 from catalog import CATALOG
 from mac import MAC
@@ -12,9 +15,9 @@ from unary import UNARY
 from testspec import specs
 from constants import *
 
-@unittest.skip("Skipped due to new design (outputs differ now!)")
 class test_MAC(unittest.TestCase):
-    '''Tests the behavior of MAC.'''
+    '''Tests the behavior of MAC.
+    '''
 
     def setUp(self):
         self.__csp = CSP()
@@ -24,35 +27,27 @@ class test_MAC(unittest.TestCase):
         self.__spec = specs["C"]
         self.__sut = MAC(self.__csp, self.__catalog, self.__spec)
         
-    def test_X_propagates_after_unary(self):
+    def test_propagate_makes_consistent(self):
         # arrange
         csp = self.__csp
-        UNARY.unarify(csp, self.__catalog, self.__spec)
+        spec = self.__spec
+        catalog = self.__catalog
+        UNARY.init_domains(csp, catalog)
+        UNARY.unarify(csp, spec)
+        D_backup = copy.deepcopy(csp.get_domains())
         # act
         X = csp.get_variables()
-        res = self.__sut.propagate(X)
+        res = self.__sut.propagate(copy.deepcopy(X))
         # assess
-        self.assertEqual(res[0], PROPAGATION_PROCEEDED)
-        expected = csp.get_constraints().keys()
-        self.assertEqual(res[2], expected)
-
-    def test_R1_gets_established(self):
-        # arrange
-        csp = self.__csp
-        X = csp.get_variables()
-        UNARY.unarify(csp, self.__catalog, self.__spec)
-        self.__sut.propagate(X)
-        # act
-        csp.assign("R1", 0.0)
-        res = self.__sut.establish("R1", 0.0)
-        # assess
+        self.assertEqual(res[0], MADE_CONSISTENT)
+        not_expected = {"T1", "T2", "T3", "T4", "T5", "T6", "T7"}
+        not_expected.update({"R1", "R2", "R3", "R4", "R5", "R6", "R7"})
+        not_expected.add("D1")
+        expected = X.difference(not_expected)
+        self.assertEqual(res[1], expected)
         D = csp.get_domains()
-        expected_examined = {'R2','R3','R4','R5','R6','R7','T1','D1','L1'}
-        self.assertEqual(res[1], expected_examined)
-        self.assertEqual(res[3], {"stock1", "sameround"})
+        for v in expected:
+            self.assertNotEqual(D_backup[v], D[v])
 
 if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    loader = unittest.defaultTestLoader 
-    suite = loader.loadTestsFromTestCase(test_MAC)
-    runner.run(suite)
+    unittest.main()

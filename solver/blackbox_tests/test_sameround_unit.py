a633882c3bed9ba5ff4ac1cefe24d54ce20661bf
Yoones Mahdian <MahdianYoones@gmail.com>
2022-12-27 21:29:28 +0330

    Upgrades sameround to work with new variables

--- solver/blackbox_tests/test_sameround_unit.py
+++ solver/blackbox_tests/test_sameround_unit.py
@@ -23,29 +23,34 @@ class test_SAMEROUND(unittest.TestCase):
         csp = self.__csp
         assert_constraint = self.__case_runner.assert_constraint
         given = {
-            "A": {"R2": 2.5},
+            "A": {"P2": (1,1,1,2.5,1)},
             "D": {
-                "R1": {2.5, 2},
-                "R2": {2.5, 5, 4},
-                "R3": {0, 2.5},
-                "R4": {0, 2.5},
-                "R5": {0, 2.5},
-                "R6": {0, 2.5},
-                "R7": {0, 2.5}
+                "P1": {(1,1,1,2,1), (1,1,1,2.5,1)},
+                "P2": {(1,1,1,5,1), (1,1,1,2.5,1), (1,1,1,4,1)},
+                "P3": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P4": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P5": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P6": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P7": {(1,1,1,0,1), (1,1,1,2.5,1)}
             },
-            "curvar": "R2",
-            "value": 2.5
+            "curvar": "P2",
+            "value": (1,1,1,2.5,1),
+            "participants": {"P1", "P2", "P3", "P4", "P5", "P6", "P7"}
         }
         expect = {
-            "out": (MADE_CONSISTENT, {"R1", "R3", "R4", "R5", "R6", "R7"}),
+            "out": (MADE_CONSISTENT, {"P1", "P3", "P4", "P5", "P6", "P7"}),
             "D": {
-                "R1": {2.5},
-                "R2": {2.5, 5, 4},
-                "R3": {2.5},
-                "R4": {2.5},
-                "R5": {2.5},
-                "R6": {2.5},
-                "R7": {2.5}
+                "P1": {(1,1,1,2.5,1)},
+                "P2": {
+                    (1,1,1,2.5,1),
+                    (1,1,1,5,1),
+                    (1,1,1,4,1)
+                },
+                "P3": {(1,1,1,2.5,1)},
+                "P4": {(1,1,1,2.5,1)},
+                "P5": {(1,1,1,2.5,1)},
+                "P6": {(1,1,1,2.5,1)},
+                "P7": {(1,1,1,2.5,1)}
             }
         }
         assert_constraint(csp, sut, "establish", given, expect)
@@ -55,28 +60,29 @@ class test_SAMEROUND(unittest.TestCase):
         csp = self.__csp
         assert_constraint = self.__case_runner.assert_constraint
         given = {
-            "A": {"R2": 2.5},
+            "A": {"P2": (1,1,1,2.5,1)},
             "D": {
-                "R1": {0},
-                "R2": {2.5},
-                "R3": {0, 2.5},
-                "R4": {0, 2.5},
-                "R5": {0, 2.5},
-                "R6": {0, 2.5},
-                "R7": {0, 2.5}
+                "P1": {(1,1,1,0,1)},
+                "P2": {(1,1,1,2.5,1)},
+                "P3": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P4": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P5": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P6": {(1,1,1,0,1), (1,1,1,2.5,1)},
+                "P7": {(1,1,1,0,1), (1,1,1,2.5,1)}
             },
-            "curvar": "R2",
-            "value": 2.5
+            "curvar": "P2",
+            "value": (1,1,1,2.5,1),
+            "participants": {"P1", "P2", "P3", "P4", "P5", "P6", "P7"}
         }
         expect = {
-            "out": (CONTRADICTION, {"R1", "R3", "R4", "R5", "R6", "R7"})
+            "out": (CONTRADICTION, {"P1", "P3", "P4", "P5", "P6", "P7"})
         }
         assert_constraint(csp, sut, "establish", given, expect)
     
     def test_establishes_once_only(self):
         '''Enforces the case that the algorithm simply does nothing!
         
-        The algorithm only checks domains if THE FIRST R variable is being
+        The algorithm only checks domains if THE FIRST P variable is being
         established. Otherwise, we are sure that domains are consistent AND further
         reduction is impossible.
         
@@ -85,18 +91,19 @@ class test_SAMEROUND(unittest.TestCase):
         csp = self.__csp
         assert_constraint = self.__case_runner.assert_constraint
         given = {
-            "A": {"R1": 2.5},
+            "A": {"P1": (1,1,1,2.5,1)},
             "D": {
-                "R1": {2.5},
-                "R2": {2.5},
-                "R3": {2.5},
-                "R4": {2.5},
-                "R5": {2.5},
-                "R6": {2.5},
-                "R7": {2.5}
+                "P1": {(1,1,1,2.5,1)},
+                "P2": {(1,1,1,2.5,1)},
+                "P3": {(1,1,1,2.5,1)},
+                "P4": {(1,1,1,2.5,1)},
+                "P5": {(1,1,1,2.5,1)},
+                "P6": {(1,1,1,2.5,1)},
+                "P7": {(1,1,1,2.5,1)}
             },
-            "curvar": "R2",
-            "value": 2.5
+            "curvar": "P2",
+            "value": (1,1,1,2.5,1),
+            "participants": {"P1", "P2", "P3", "P4", "P5", "P6", "P7"}
         }
         expect = {
             "out": REVISED_NONE
@@ -109,16 +116,17 @@ class test_SAMEROUND(unittest.TestCase):
         assert_constraint = self.__case_runner.assert_constraint
         given = {
             "D": {
-                "R1": {2.5},
-                "R2": {2.5},
-                "R3": {2.5},
-                "R4": {2.5},
-                "R5": {2.5},
-                "R6": {2.5},
-                "R7": {2.5}
+                "P1": {(1,1,1,2.5,1)},
+                "P2": {(1,1,1,2.5,1)},
+                "P3": {(1,1,1,2.5,1)},
+                "P4": {(1,1,1,2.5,1)},
+                "P5": {(1,1,1,2.5,1)},
+                "P6": {(1,1,1,2.5,1)},
+                "P7": {(1,1,1,2.5,1)}
             },
-            "curvar": "R1",
-            "value": 2.5
+            "curvar": "P1",
+            "value": (1,1,1,2.5,1),
+            "participants": {"P1", "P2", "P3", "P4", "P5", "P6", "P7"}
         }
         expect = {
             "out": ALREADY_CONSISTENT
--- solver/sameround.py
+++ solver/sameround.py
@@ -1,88 +1,39 @@
 from constants import *
 
 class SAMEROUND():
-    '''Applies same roundness constraints.
-    
-    The variables R1 throught R7 must have the same value in the
-    final solution.
-
-    R1 through R7 must be assigned the same value. If at any point, no
-    intersection between the domains of these variables exists, contradiction
-    has been occured.
-
-    Having no intersection between the domains means that the constraint
-    cannot be satisfied.
-
-    However, given that R1 is assigned first before other variables, we
-    can implement a simpler and more efficient algorithm.
-
-    Upon assigning any value to R1, we clean the domain of other R variables
-    and only leave the assigned value.
-
-    For example, if R1 is assigned 0.5mm and other domains are:
-
-    R2 = {0, 0.5, 1, 1.5}
-    R3 = {0.5, 1, 1.5, 2}
-    R4 = {0.5, 2}
-    and etc
-
-    the domains will be come as such:
-
-    R2 = {0.5}
-    R3 = {0.5}
-    R4 = {0.5}
-    and etc
-
-    and the contradiction can be easily detected if the assigned value cannot
-    be found in the current domain of any R variable.
-
-    Note that after assignment of R1 and consistency, propagation does not 
-    occur since the domains contain only one value. If any other algorithm
-    removes this single value, it issues contradiction itself.'''
+    '''Applies same roundness constraints.'''
     
     def propagate(self, csp, reduced_vars, participants):
         '''Establishes indirect consistency W.R.T. same_r.'''
-        D = csp.get_domains()
-        R_vars = {"R1", "R2", "R3", "R4", "R5", "R6", "R7"}
-        if R_vars.intersection(csp.get_assigned_vars()) != set([]):
-            return REVISED_NONE
-        common_values = D["R1"]
-        R_vars.remove("R1")
-        for v in R_vars:
-            common_values = common_values.intersection(D[v])
-        if common_values == set([]):
-            return CONTRADICTION, self.__failed_set(csp)
-        reduced_vars = set([])
-        for v in R_vars:
-            if common_values != D[v] and len(common_values) < len(D[v]):
-                reduced_vars.add(v)
-                csp.update_domain(v, common_values)
-        if reduced_vars != set([]):
-            return MADE_CONSISTENT, reduced_vars
-        return ALREADY_CONSISTENT
+        return REVISED_NONE
     
-    def __new_domains(self, value, D, examined):
+    def __new_domains(self, value, D, revising_vars):
         '''Returns new domains for participating variables.'''
         newdomains = {}
-        for _var in examined:
-            if not value in D[_var]:
+        assigned_roundness = value[3]
+        for revising_var in revising_vars:
+            newdomains[revising_var] = set([])
+            for piece in D[revising_var]:
+                if piece[3] == assigned_roundness:
+                    newdomains[revising_var].add(piece)
+            if len(newdomains[revising_var]) == 0:
                 return CONTRADICTION
-            elif len(D[_var]) == 1:
+            elif D[revising_var] == newdomains[revising_var]:
+                del newdomains[revising_var]
                 continue
-            newdomains[_var] = {value}
         return newdomains
              
     def establish(self, csp, curvar, value, participants):
         '''Establishes consistency after assignment curvar: value.'''
         A = csp.get_assignment()
-        examined = set([])
-        for v in {"R1", "R2", "R3", "R4", "R5", "R6", "R7"}:
+        revising_vars = set([])
+        for v in participants: # P1 thorugh P7
             if v in A and v != curvar:
                 return REVISED_NONE
             if not v in A and v != curvar:
-                examined.add(v)
+                revising_vars.add(v)
         D = csp.get_domains()
-        newdomains = self.__new_domains(value, D, examined)
+        newdomains = self.__new_domains(value, D, revising_vars)
         reduced = set([])
         if newdomains == CONTRADICTION:
             return (CONTRADICTION, self.__failed_set(csp))
@@ -95,6 +46,6 @@ class SAMEROUND():
 
     def __failed_set(self, csp):
         '''Returns the failed set.'''
-        participants = {"R1", "R2", "R3", "R4", "R5", "R6", "R7"}
+        participants = {"P1", "P2", "P3", "P4", "P5", "P6", "P7"}
         unassigned = csp.get_unassigned_vars()
         return participants.intersection(unassigned)            

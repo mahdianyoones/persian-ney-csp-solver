63ad0c7f7571026d1d274918850f40474bd46201
Yoones Mahdian <MahdianYoones@gmail.com>
2023-03-07 12:10:28 +0330

    Removes the containing files in the solver directory into the root directory

--- pnconst/webui/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Reedpiece(models.Model):
-    label = models.CharField(max_length=255)
-    length = models.FloatField()
-    diam = models.FloatField()
-    thickness = models.FloatField()
-    roundness = models.FloatField()
-    

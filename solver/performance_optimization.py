5bd21b1e332fcbe1c20cd46496318f329cfe23cf
Yoones Mahdian <MahdianYoones@gmail.com>
2023-03-07 11:55:08 +0330

    Updates performance_optimization script to handle multiple solutions

--- solver/performance_optimization.py
+++ solver/performance_optimization.py
@@ -6,35 +6,7 @@ current = op.dirname(op.realpath(__file__))
 parent = op.dirname(current)
 sp.append(parent)
 
-from solver import SOLVER
-from csp import CSP
-from spec import specs
-from catalog import CATALOG
-from unary import UNARY
-from mac import MAC
-from pickup import SELECT
-from constants import *
-
-NO_SOLUTION_DS = "contains_no_solution"
-SOLUTION_DS = "contains_solutions"
-REAL_DS = "real_pieces"
-
-def __find(catalog, kook, csp = None):
-    '''Generalises arrange and act of all test cases in this suite.'''
-    if csp == None:
-        csp = CSP()
-    select = SELECT(csp)
-    mac = MAC(csp, catalog, specs[kook])
-    UNARY.init_domains(csp, catalog)
-    UNARY.unarify(csp, specs[kook])
-    solver = SOLVER(csp, select, mac)
-    res = solver.find_independent(catalog, specs[kook])
-    return res
-
-def main():
-    data_set_path = current+"/pieces.csv"
-    catalog = CATALOG(data_set_path)
-    res = __find(catalog, "C")
+from bb import main
 
 if __name__ == "__main__":
     cProfile.run("main()", "speed_profile")

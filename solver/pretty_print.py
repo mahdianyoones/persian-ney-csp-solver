3c401963ff17dfa86b47aabdd7453ff38412754b
Yoones Mahdian <MahdianYoones@gmail.com>
2023-03-07 11:55:33 +0330

    Pretty print now adds a horizontal delimiter between each print

--- solver/pretty_print.py
+++ solver/pretty_print.py
@@ -1,4 +1,5 @@
 def print_solution(solution):
+    print("--------------------")
     all_vars = sorted(solution.keys(), key=lambda n: int(n[1:]))
     pvars_sorted = [pvar for pvar in all_vars if pvar[0] == "P"]
     lvars_sorted = [lvar for lvar in all_vars if lvar[0] == "L"]
